83fb94077b88e2b277c4a18ccfba50b13e6d8d50
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2013-10-18 00:27:47 +0200

    przywrócenie wersji pliku sprzed commita - niepotrzebne zmiany na importach

--- javers-core/src/main/java/org/javers/model/mapping/BeanProperty.java
+++ javers-core/src/main/java/org/javers/model/mapping/BeanProperty.java
@@ -1,13 +1,15 @@
 package org.javers.model.mapping;
 
 import org.javers.common.reflection.ReflectionUtil;
+import org.javers.common.validation.Validate;
+import org.javers.common.validation.Validate;
 import org.javers.model.mapping.type.JaversType;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.persistence.Id;
 import java.lang.reflect.Method;
 
-import static org.javers.common.validation.Validate.argumentIsNotNull;
+import static org.javers.common.validation.Validate.*;
 
 /**
  * Immutable
@@ -20,6 +22,7 @@ public class BeanProperty implements Property {
     private final JaversType javersType;
 
     protected BeanProperty(Method getter, JaversType javersType) {
+
         argumentIsNotNull(getter, "getter should not be null!");
         argumentIsNotNull(javersType, "javersType should not be null!");
 
