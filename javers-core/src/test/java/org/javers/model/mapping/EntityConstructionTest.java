36cce755c24d674c113a633bcdd5ae00fb6e25af
bartosz.walacik <bartosz.walacik@allegro.pl>
2013-10-20 18:23:25 +0200

    shouldScanAllProperties() comes back

--- javers-core/src/test/java/org/javers/model/mapping/EntityConstructionTest.java
+++ javers-core/src/test/java/org/javers/model/mapping/EntityConstructionTest.java
@@ -1,6 +1,7 @@
 package org.javers.model.mapping;
 
 import org.javers.core.model.DummyUser;
+import org.javers.test.assertion.Assertions;
 import org.testng.annotations.Test;
 
 
@@ -18,4 +19,16 @@ public abstract class EntityConstructionTest {
         //then
         EntityAssert.assertThat(entity).hasSourceClass(DummyUser.class);
     }
+
+     @Test
+     public void shouldScanAllProperties() {
+        //when
+        Entity entity = entityFactory.createEntity(DummyUser.class);
+
+        //then
+        Assertions.assertThat(entity.getProperties()).hasSize(14);
+
+     }
+
+
 }
