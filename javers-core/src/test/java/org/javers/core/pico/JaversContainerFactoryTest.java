a6fc269d7839cf3fbac9dddaa544e03b3532abb1
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2013-10-23 23:41:23 +0200

    cofniecie niepotrzebnych zmian na importach

--- javers-core/src/test/java/org/javers/core/pico/JaversContainerFactoryTest.java
+++ javers-core/src/test/java/org/javers/core/pico/JaversContainerFactoryTest.java
@@ -2,10 +2,7 @@ package org.javers.core.pico;
 
 import org.javers.core.Javers;
 import org.javers.core.MappingStyle;
-import org.javers.model.mapping.BeanBasedPropertyScanner;
-import org.javers.model.mapping.EntityManager;
-import org.javers.model.mapping.FieldBasedPropertyScanner;
-import org.javers.model.mapping.PropertyScanner;
+import org.javers.model.mapping.*;
 import org.javers.model.mapping.type.TypeMapper;
 import org.picocontainer.PicoContainer;
 import org.testng.annotations.Test;
