1906b997a56321a2fae24e7b8d1a254d5c877eae
bartosz.walacik <bartosz.walacik@allegro.pl>
2013-10-26 23:56:48 +0200

    simplified GlobalCdoId.equals()

--- javers-core/src/main/java/org/javers/model/domain/GlobalCdoId.java
+++ javers-core/src/main/java/org/javers/model/domain/GlobalCdoId.java
@@ -37,16 +37,13 @@ public class GlobalCdoId {
         if (obj == null || obj.getClass() != getClass()) {
             return false;
         }
-        if (obj == this) {
-            return true;
-        }
         GlobalCdoId other = (GlobalCdoId) obj;
-        return cdoId.equals(other.cdoId) && entity.getSourceClass().equals(other.entity.getSourceClass());
+        return cdoId.equals(other.cdoId) && entity.equals(other.entity);
     }
 
     @Override
     public int hashCode() {
-        int result = entity.getSourceClass().hashCode();
+        int result = entity.hashCode();
         result = 31 * result + cdoId.hashCode();
         return result;
     }
