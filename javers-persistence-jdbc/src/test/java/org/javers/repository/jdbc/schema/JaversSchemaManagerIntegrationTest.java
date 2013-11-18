95a6b2dbced10efaf5d354747d50dd697cf23410
bartosz.walacik <bartosz.walacik@allegro.pl>
2013-11-18 17:46:31 +0100

    removed missing dependency in pom.xml

--- javers-persistence-jdbc/src/test/java/org/javers/repository/jdbc/schema/JaversSchemaManagerIntegrationTest.java
+++ javers-persistence-jdbc/src/test/java/org/javers/repository/jdbc/schema/JaversSchemaManagerIntegrationTest.java
@@ -4,25 +4,24 @@ import org.junit.Before;
 import org.junit.Test;
 import org.polyjdbc.core.dialect.Dialect;
 import org.polyjdbc.core.dialect.DialectRegistry;
-import org.polyjdbc.core.integration.DataSourceFactory;
-
 import javax.sql.DataSource;
 
 /**
  * @author bartosz walacik
  */
+@Deprecated
 public class JaversSchemaManagerIntegrationTest {
     private JaversSchemaManager javersSchemaManager;
 
     @Before
     public void before() {
         Dialect dialect = DialectRegistry.H2.getDialect();
-        DataSource dataSource = DataSourceFactory.create(dialect, "jdbc:h2:mem:test", "", "");
+        //DataSource dataSource = DataSourceFactory.create(dialect, "jdbc:h2:mem:test", "", "");
 
         //Dialect dialect = DialectRegistry.POSTGRES.getDialect();
         //DataSource dataSource = DataSourceFactory.create(dialect,"jdbc:postgresql://localhost/javers_devel","javers","p_javers");
 
-        javersSchemaManager = new JaversSchemaManager(dataSource, null, new FixedSchemaFactory());
+        //javersSchemaManager = new JaversSchemaManager(dataSource, null, new FixedSchemaFactory());
     }
     @Test
     public void testSchemaCreate() {
