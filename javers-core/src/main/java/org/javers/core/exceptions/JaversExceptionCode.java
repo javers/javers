928aeb8df448a7837fc4b09e896aa190e13ab01e
bartosz.walacik <bartosz.walacik@allegro.pl>
2013-11-18 09:42:54 +0100

    Merge branch 'jdbc-persistence' cd

--- javers-core/src/main/java/org/javers/core/exceptions/JaversExceptionCode.java
+++ javers-core/src/main/java/org/javers/core/exceptions/JaversExceptionCode.java
@@ -12,6 +12,7 @@ public enum JaversExceptionCode {
      * Class is not defined in Javers configuration.
      */
     CLASS_NOT_MANAGED(RUNTIME_ERROR + "Class '%s' is not managed. Add this class to your JaVers configuration."),
+    UNEXPECTED_VALUE_OBJECT(RUNTIME_ERROR + "Can't start building graph from Value Object '%s', expected Entity instance."),
     TYPE_NOT_MAPPED (BOOTSTRAP_ERROR + "Property Type '%s' is not mapped. Implement UserType and add it to your JaVers configuration.") ,
     ENTITY_WITHOUT_ID (BOOTSTRAP_ERROR + "Class '%s' has no Id property. Use @Id annotation to mark unique Entity identifier"),
     //TODO better exception messages - User Friendly!
@@ -20,6 +21,7 @@ public enum JaversExceptionCode {
     MALFORMED_PROPERTY(BOOTSTRAP_ERROR + "value '%s' is invalid for property '%s'. Fix it in your classpath:javers.properties"),
     CLASSPATH_RESOURCE_NOT_FOUND(BOOTSTRAP_ERROR + "classpath resource '%s' could not be found"),
     JAVERS_ALREADY_BUILT(BOOTSTRAP_ERROR + "JaVers instance already built, each JaversBuilder may produce only one JaVers instance");
+
 
 
     private String message;
