a20a9b471df36cd558034903856ef9a1ddeb3b02
bartosz.walacik <bartosz.walacik@allegro.pl>
2013-11-26 08:16:55 +0100

    VISITOR_FIX in DFSGraphToSetConverter

--- javers-core/src/main/java/org/javers/core/diff/DFSGraphToSetConverter.java
+++ javers-core/src/main/java/org/javers/core/diff/DFSGraphToSetConverter.java
@@ -17,34 +17,18 @@ public class DFSGraphToSetConverter implements GraphToSetConverter {
     @Override
     public Set<ObjectNode> convertFromGraph(ObjectNode graph) {
         DFSGraphTraverser dfsGraphTraverser = new DFSGraphTraverser();
-        dfsGraphTraverser.visitNode(graph);
-        return dfsGraphTraverser.getVisitedNodes();
+        graph.accept(dfsGraphTraverser);
+        return dfsGraphTraverser.visitedNodes;
     }
 
     private class DFSGraphTraverser implements Visitor {
-
         private final Set<ObjectNode> visitedNodes = new HashSet<>();
-
-        private void visitNode(ObjectNode objectNode) {
-            visitedNodes.add(objectNode);
-            for (Edge edge : objectNode.getEdges()) {
-                edge.accept(this);
-            }
-        }
-
-        private Set<ObjectNode> getVisitedNodes() {
-            return visitedNodes;
-        }
 
         @Override
         public void visit(Object object) {
-
             if(object instanceof ObjectNode) {
                 visitedNodes.add((ObjectNode) object);
             }
         }
-
-
-
     }
 }
