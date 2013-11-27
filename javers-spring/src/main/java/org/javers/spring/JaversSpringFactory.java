a5364e09110aa9a594bb059e21a760df8e41c20e
pawel.cierpiatka <pawel.cierpiatka@allegro.pl>
2013-11-27 22:58:03 +0100

    spring support

---
+++ javers-spring/src/main/java/org/javers/spring/JaversSpringFactory.java
@@ -0,0 +1,67 @@
+package org.javers.spring;
+
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.springframework.beans.factory.FactoryBean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Pawel Cierpiatka
+ */
+public class JaversSpringFactory implements FactoryBean<Javers> {
+
+    private List<Class> entityClasses = new ArrayList<>();
+
+    private List<Class> valueObject = new ArrayList<>();
+
+    private Map<Class, String> describedEntityClasses = new HashMap<>();
+
+    @Override
+    public Javers getObject() throws Exception {
+
+        JaversBuilder javersBuilder = JaversBuilder.javers();
+
+        /*
+        WAITING FOR JAVERS CREW :)
+        for (Map.Entry<Class, String> entrie : customsId.entrySet()) {
+            javersBuilder.registerEntityWithCustomId(entrie.getKey(), entrie.getValue());
+        }*/
+
+        for(Class clazz : entityClasses) {
+            javersBuilder.registerEntity(clazz);
+        }
+
+        for(Class clazz : entityClasses) {
+            javersBuilder.registerValueObject(clazz);
+        }
+
+        return javersBuilder.build();
+    }
+
+
+    @Override
+    public Class<?> getObjectType() {
+        return Javers.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public void setEntityClasses(List<Class> entityClasses) {
+        this.entityClasses = entityClasses;
+    }
+
+    public void setValueObject(List<Class> valueObject) {
+        this.valueObject = valueObject;
+    }
+
+    public void setDescribedEntityClasses(Map<Class, String> describedEntityClasses) {
+        this.describedEntityClasses = describedEntityClasses;
+    }
+}
---
+++ javers-spring/src/test/org.javers.spring/JaversSpringFactoryTest.java
@@ -0,0 +1,30 @@
+/**
+ * @author Pawel Cierpiatka
+ */
+public class JaversSpringFactoryTest {
+
+    public void shouldRegistredEntity() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    public void shouldRegistredDescriptedentityClass() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    public void shouldRegistredValueObject() {
+        //given
+
+        //when
+
+        //then
+    }
+
+}
