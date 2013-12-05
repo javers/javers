9d68806d57c4211a48433c2986271f776ace2e18
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2013-12-05 16:00:35 +0100

    more groove in groove

--- javers-core/src/test/groovy/org/javers/model/mapping/EntityManagerTest.groovy
+++ javers-core/src/test/groovy/org/javers/model/mapping/EntityManagerTest.groovy
@@ -18,14 +18,14 @@ import static org.mockito.Mockito.mock
 
 class EntityManagerTest extends Specification{
 
-    private EntityManager entityManager;
+    private EntityManager entityManager
 
     def setup() {
-        TypeMapper mapper = new TypeMapper();
-        BeanBasedPropertyScanner scanner = new BeanBasedPropertyScanner(mapper);
-        EntityFactory entityFactory = new EntityFactory(scanner);
-        ValueObjectFactory valueObjectFactory = new ValueObjectFactory();
-        entityManager = new EntityManager(entityFactory, valueObjectFactory, mapper);
+        TypeMapper mapper = new TypeMapper()
+        BeanBasedPropertyScanner scanner = new BeanBasedPropertyScanner(mapper)
+        EntityFactory entityFactory = new EntityFactory(scanner)
+        ValueObjectFactory valueObjectFactory = new ValueObjectFactory()
+        entityManager = new EntityManager(entityFactory, valueObjectFactory, mapper)
     }
 
     def "should throw exception if entity is not managed when trying to get it"() {
@@ -46,8 +46,8 @@ class EntityManagerTest extends Specification{
         CatchException.catchException(entityManager).getByClass(DummyManagedClass)
 
         then:
-        assertThat(caughtException()).overridingErrorMessage("No exception caught").isNotNull();
-        assertThat((JaversException) caughtException()).hasCode(JaversExceptionCode.ENTITY_MANAGER_NOT_INITIALIZED);
+        assertThat(caughtException()).overridingErrorMessage("No exception caught").isNotNull()
+        assertThat((JaversException) caughtException()).hasCode(JaversExceptionCode.ENTITY_MANAGER_NOT_INITIALIZED)
     }
 
     def "should throw exception when class is mapped but is not instance of ManagedClass"() {
@@ -55,8 +55,8 @@ class EntityManagerTest extends Specification{
         CatchException.catchException(entityManager).getByClass(Integer)
 
         then:
-        assertThat(caughtException()).overridingErrorMessage("No exception caught").isNotNull();
-        assertThat((JaversException) caughtException()).hasCode(JaversExceptionCode.EXPECTED_ENTITY_OR_VALUE_OBJECT_SOURCE_CLASS);
+        assertThat(caughtException()).overridingErrorMessage("No exception caught").isNotNull()
+        assertThat((JaversException) caughtException()).hasCode(JaversExceptionCode.EXPECTED_ENTITY_OR_VALUE_OBJECT_SOURCE_CLASS)
     }
 
     def "should return entity model for managed class after Building it"() {
@@ -87,41 +87,41 @@ class EntityManagerTest extends Specification{
 
     def "should return true for managed ValueObject"() {
         given:
-        entityManager.registerValueObject(DummyNetworkAddress.class)
+        entityManager.registerValueObject(DummyNetworkAddress)
         entityManager.buildManagedClasses()
 
         when:
-        boolean isManaged = entityManager.isManaged(DummyNetworkAddress.class)
+        boolean isManaged = entityManager.isManaged(DummyNetworkAddress)
 
         then:
-        assertThat(isManaged).isTrue();
+        assertThat(isManaged).isTrue()
     }
 
     def "should not register entity in type mapper more than once"() {
         given:
         TypeMapper typeMapper = new TypeMapper()
         EntityDefinition managedClass = new EntityDefinition(DummyManagedClass)
-        EntityManager entityManager = new EntityManager(mock(EntityFactory.class), mock(ValueObjectFactory.class), typeMapper)
+        EntityManager entityManager = new EntityManager(mock(EntityFactory), mock(ValueObjectFactory), typeMapper)
 
         when:
         entityManager.registerEntity(managedClass)
         entityManager.registerEntity(managedClass)
 
         then:
-        Assertions.assertThat(typeMapper.getMappedEntityReferenceTypes()).hasSize(1);
+        Assertions.assertThat(typeMapper.getMappedEntityReferenceTypes()).hasSize(1)
     }
 
     def "should not register value object more than once"() {
         given:
-        Class alreadyMappedValueObject = DummyNetworkAddress.class;
-        TypeMapper typeMapper = new TypeMapper();
-        EntityManager entityManager = new EntityManager(mock(EntityFactory.class), mock(ValueObjectFactory.class), typeMapper);
+        Class alreadyMappedValueObject = DummyNetworkAddress
+        TypeMapper typeMapper = new TypeMapper()
+        EntityManager entityManager = new EntityManager(mock(EntityFactory), mock(ValueObjectFactory), typeMapper)
 
         when:
-        entityManager.registerValueObject(alreadyMappedValueObject);
-        entityManager.registerValueObject(alreadyMappedValueObject);
+        entityManager.registerValueObject(alreadyMappedValueObject)
+        entityManager.registerValueObject(alreadyMappedValueObject)
 
         then:
-        Assertions.assertThat(typeMapper.getMappedValueObjectTypes()).hasSize(1);
+        Assertions.assertThat(typeMapper.getMappedValueObjectTypes()).hasSize(1)
     }
 }
