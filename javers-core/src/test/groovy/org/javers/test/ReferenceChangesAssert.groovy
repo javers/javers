e09cf12379dc90c7005c2d0222e093fdccacb41d
bartosz.walacik <bartosz.walacik@allegro.pl>
2013-12-17 23:28:55 +0100

    more groovy in groovy

--- javers-core/src/test/groovy/org/javers/test/ReferenceChangesAssert.groovy
+++ javers-core/src/test/groovy/org/javers/test/ReferenceChangesAssert.groovy
@@ -4,68 +4,59 @@ import org.javers.model.domain.GlobalCdoId
 import org.javers.model.domain.changeType.ReferenceChanged
 import org.javers.model.mapping.Property
 
-
 class ReferenceChangesAssert {
 
-    def Collection<ReferenceChanged> actual;
+    Collection<ReferenceChanged> actual
 
-    private ReferenceChangesAssert(Collection<ReferenceChanged> actual) {
-        this.actual = actual;
+    static ReferenceChangesAssert assertThat(Collection<ReferenceChanged> actual) {
+        new ReferenceChangesAssert(actual:actual)
     }
 
-    def static ReferenceChangesAssert assertThat(Collection<ReferenceChanged> actual) {
-        return new ReferenceChangesAssert(actual)
-    }
-
-    def ReferenceChangesAssert hasSize(int expected) {
+    ReferenceChangesAssert hasSize(int expected) {
         assert actual.size() == expected
         return this;
     }
 
-    def ReferenceChangeAssert assertThatFirstChange() {
+    ReferenceChangeAssert assertThatFirstChange() {
         return ReferenceChangeAssert.assertThat(actual.iterator().next())
     }
 
     static class ReferenceChangeAssert {
 
-        def ReferenceChanged actual;
+        ReferenceChanged actual
 
-        private ReferenceChangeAssert(ReferenceChanged actual) {
-            this.actual = actual;
+        static ReferenceChangeAssert assertThat(ReferenceChanged actual) {
+            new ReferenceChangeAssert(actual:actual)
         }
 
-        def static ReferenceChangeAssert assertThat(ReferenceChanged actual) {
-            new ReferenceChangeAssert(actual)
-        }
-
-        def hasProperty(Property expected) {
+        ReferenceChanged hasProperty(Property expected) {
             assert actual.property == expected
             this
         }
 
-        def hasCdoId(Object expected) {
+        ReferenceChanged hasCdoId(Object expected) {
             assert actual.globalCdoId.localCdoId == expected
             this
         }
 
-        def hasLeftReference(Class expectedClass, Object expectedCdoId) {
+        ReferenceChanged hasLeftReference(Class expectedClass, Object expectedCdoId) {
             assert actual.leftReference.localCdoId == expectedCdoId
             assert actual.leftReference.entity.sourceClass == expectedClass
             this
         }
 
-        def hasRightReference(Class expectedClass, Object expectedCdoId) {
+        ReferenceChanged hasRightReference(Class expectedClass, Object expectedCdoId) {
             assert actual.rightReference.localCdoId == expectedCdoId
             assert actual.rightReference.entity.sourceClass == expectedClass
             this
         }
 
-        def hasLeftReference(GlobalCdoId expected) {
+        ReferenceChanged hasLeftReference(GlobalCdoId expected) {
             assert actual.leftReference == expected
             this
         }
 
-        def hasRightReference(GlobalCdoId expected) {
+        ReferenceChanged hasRightReference(GlobalCdoId expected) {
             assert actual.rightReference == expected
             this
         }
