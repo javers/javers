e75407c760ed9f9160884a6fdddde71f7e323301
bartosz.walacik <bwalacik@gmail.com>
2014-03-08 23:09:25 +0100

    lot of minor sonar-like fixes

---
+++ javers-core/src/main/java/org/javers/common/properties/AbstractConfiguration.java
@@ -0,0 +1,37 @@
+package org.javers.common.properties;
+
+/**
+ * @author bartosz walacik
+ */
+public abstract class AbstractConfiguration {
+    private PropertyConfiguration propertyConfiguration;
+
+    public AbstractConfiguration(PropertyConfiguration propertyConfiguration) {
+        this.propertyConfiguration = propertyConfiguration;
+    }
+
+    public void readProperties(String classpathName) {
+        propertyConfiguration = new PropertyConfiguration(classpathName);
+        assemble();
+    }
+
+    protected <T extends Enum<T>> T getEnumProperty(String propertyKey, Class<T> enumType) {
+        return propertyConfiguration.getEnumProperty(propertyKey, enumType);
+    }
+
+    protected String getStringProperty(String propertyKey) {
+        return propertyConfiguration.getStringProperty(propertyKey);
+    }
+
+    public boolean containsPropertyKey(String propertyKey) {
+        return propertyConfiguration.contains(propertyKey);
+    }
+
+    /**
+     * Called after reading properties file,
+     * override to read configuration from it.
+     * <br/>
+     * Should not override property to null if propertyKey is not found in file
+     */
+    protected abstract void assemble();
+}
