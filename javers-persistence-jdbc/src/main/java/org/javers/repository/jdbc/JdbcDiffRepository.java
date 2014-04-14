65511334b5390f52da19bbe772d4187c7804f80f
bartosz.walacik <bwalacik@gmail.com>
2014-04-14 09:34:31 +0200

    messy commit

--- javers-persistence-jdbc/src/main/java/org/javers/repository/jdbc/JdbcDiffRepository.java
+++ javers-persistence-jdbc/src/main/java/org/javers/repository/jdbc/JdbcDiffRepository.java
@@ -2,8 +2,7 @@ package org.javers.repository.jdbc;
 
 import org.javers.common.collections.Optional;
 import org.javers.core.commit.Commit;
-import org.javers.core.diff.Change;
-import org.javers.core.diff.Diff;
+import org.javers.core.commit.CommitId;
 import org.javers.core.metamodel.object.CdoSnapshot;
 import org.javers.core.metamodel.object.GlobalCdoId;
 import org.javers.repository.api.JaversRepository;
@@ -14,6 +13,12 @@ import java.util.List;
  * @author bartosz walacik
  */
 public class JdbcDiffRepository implements JaversRepository {
+
+    @Override
+    public List<CdoSnapshot> getStateHistory(GlobalCdoId globalId, int limit) {
+        return null;
+    }
+
     @Override
     public Optional<CdoSnapshot> getLatest(GlobalCdoId globalId) {
         return null;
@@ -22,5 +27,10 @@ public class JdbcDiffRepository implements JaversRepository {
     @Override
     public void persist(Commit commit) {
 
+    }
+
+    @Override
+    public CommitId getHeadId() {
+        return null;
     }
 }
