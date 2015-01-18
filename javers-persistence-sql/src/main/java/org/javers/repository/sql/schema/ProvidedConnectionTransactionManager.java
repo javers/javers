56ca8763b134a50d0c29e1cc2ff375bd2978999f
bartosz.walacik <bwalacik@gmail.com>
2015-01-18 13:27:36 +0100

    https://github.com/javers/javers/issues/85 forking poly

---
+++ javers-persistence-sql/src/main/java/org/javers/repository/sql/schema/ProvidedConnectionTransactionManager.java
@@ -0,0 +1,29 @@
+package org.javers.repository.sql.schema;
+
+import org.javers.common.validation.Validate;
+import org.javers.repository.sql.ConnectionProvider;
+import org.polyjdbc.core.transaction.Transaction;
+import org.polyjdbc.core.transaction.TransactionManager;
+
+/**
+ * @author bartosz walacik
+ */
+public class ProvidedConnectionTransactionManager implements TransactionManager {
+
+    private final ConnectionProvider connectionProvider;
+
+    public ProvidedConnectionTransactionManager(ConnectionProvider connectionProvider) {
+        this.connectionProvider = connectionProvider;
+    }
+
+    @Override
+    public Transaction openTransaction() {
+        return openTransaction(false);
+    }
+
+    @Override
+    public Transaction openTransaction(boolean autoCommit) {
+        Validate.argumentCheck(autoCommit == false, "autoCommit is not supported");
+        return new Transaction(connectionProvider.getConnection());
+    }
+}
---
+++ javers-polyjdbc/src/main/java/org/polyjdbc/core/transaction/DataSourceTransactionManager.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013 Adam Dubiel, Przemek Hertel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.polyjdbc.core.transaction;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.polyjdbc.core.exception.PolyJdbcException;
+
+/**
+ *
+ * @author Adam Dubiel
+ */
+public class DataSourceTransactionManager implements TransactionManager {
+
+    private DataSource dataSource;
+
+    public DataSourceTransactionManager(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Transaction openTransaction() {
+        return openTransaction(false);
+    }
+
+    @Override
+    public Transaction openTransaction(boolean autoCommit) {
+        Connection connection;
+        try {
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(autoCommit);
+            return new Transaction(connection);
+        } catch (SQLException e) {
+            throw new PolyJdbcException("OPEN_CONNECTION_ERROR", "Failed to obtain connection from datasource.", e);
+        }
+    }
+}
