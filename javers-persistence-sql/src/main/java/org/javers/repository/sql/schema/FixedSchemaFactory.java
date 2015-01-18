dafaccf23f36c27e0c1df3a0266aa4da1b00e70f
bartosz.walacik <bwalacik@gmail.com>
2015-01-18 16:10:55 +0100

    https://github.com/javers/javers/issues/85 full schema

--- javers-persistence-sql/src/main/java/org/javers/repository/sql/schema/FixedSchemaFactory.java
+++ javers-persistence-sql/src/main/java/org/javers/repository/sql/schema/FixedSchemaFactory.java
@@ -14,6 +14,12 @@ import java.util.TreeMap;
  */
 public class FixedSchemaFactory {
 
+    public static final String SNAP_PROPERTY_TABLE_NAME =  "jv_snapshot_property";
+    public static final String SNAP_PROPERTY_PK =          "snapshot_property_pk";
+    public static final String SNAP_PROPERTY_SNAPSHOT_FK = "snapshot_fk";
+    public static final String SNAP_PROPERTY_NAME =         "name";
+    public static final String SNAP_PROPERTY_VALUE =        "value";
+
     public static final String CDO_CLASS_TABLE_NAME = "jv_cdo_class";
     public static final String CDO_CLASS_PK =         "cdo_class_pk";
     public static final String CDO_CLASS_QUALIFIED_NAME = "qualified_name";
@@ -25,11 +31,30 @@ public class FixedSchemaFactory {
 
     public static final String COMMIT_TABLE_NAME =    "jv_commit";
     public static final String COMMIT_TABLE_PK =      "commit_pk";
+    public static final String COMMIT_TABLE_AUTHOR =  "author";
+    public static final String COMMIT_TABLE_COMMIT_DATE = "commit_date";
+    public static final String COMMIT_TABLE_COMMIT_ID =   "commit_id";
 
     public static final String SNAPSHOT_TABLE_NAME = "jv_snapshot";
     public static final String SNAPSHOT_TABLE_PK =   "snapshot_pk";
     public static final String SNAPSHOT_TABLE_COMMIT_FK = "commit_fk";
     public static final String SNAPSHOT_TABLE_GLOBAL_ID_FK = "global_id_fk";
+
+
+    private Schema snapshotPropertyTableSchema(Dialect dialect, String tableName){
+        Schema schema = new Schema(dialect);
+        RelationBuilder relationBuilder = schema.addRelation(tableName);
+        primaryKey(tableName, SNAP_PROPERTY_PK, schema, relationBuilder);
+        relationBuilder
+                .withAttribute().string(SNAP_PROPERTY_NAME).withMaxLength(200).and()
+                .withAttribute().text(SNAP_PROPERTY_VALUE).and();
+        foreignKey(tableName, SNAP_PROPERTY_SNAPSHOT_FK, SNAPSHOT_TABLE_NAME, SNAPSHOT_TABLE_PK, relationBuilder, schema);
+        relationBuilder.build();
+
+        foreignKeyIndex(tableName, SNAP_PROPERTY_SNAPSHOT_FK, schema);
+
+        return schema;
+    }
 
     private Schema snapshotTableSchema(Dialect dialect, String tableName){
         Schema schema = new Schema(dialect);
@@ -50,9 +75,9 @@ public class FixedSchemaFactory {
         RelationBuilder relationBuilder = schema.addRelation(tableName);
         primaryKey(tableName, COMMIT_TABLE_PK,schema,relationBuilder);
         relationBuilder
-                .withAttribute().string("author").withMaxLength(200).and()
-                .withAttribute().timestamp("commit_date").notNull().and()
-                .withAttribute().string("commit_id").withMaxLength(10).and()
+                .withAttribute().string(COMMIT_TABLE_AUTHOR).withMaxLength(200).and()
+                .withAttribute().timestamp(COMMIT_TABLE_COMMIT_DATE).and()
+                .withAttribute().string(COMMIT_TABLE_COMMIT_ID).withMaxLength(10).and()
                 .build();
         return schema;
     }
@@ -71,7 +96,7 @@ public class FixedSchemaFactory {
         RelationBuilder relationBuilder = schema.addRelation(tableName);
         primaryKey(tableName, GLOBAL_ID_PK, schema,relationBuilder);
         relationBuilder
-                .withAttribute().text(GLOBAL_ID_LOCAL_ID).notNull();
+                .withAttribute().text(GLOBAL_ID_LOCAL_ID).and();
         foreignKey(tableName, GLOBAL_ID_CLASS_FK, CDO_CLASS_TABLE_NAME, CDO_CLASS_PK, relationBuilder, schema);
         relationBuilder.build();
 
@@ -102,8 +127,9 @@ public class FixedSchemaFactory {
 
         schema.put(CDO_CLASS_TABLE_NAME, cdoClassTableSchema(dialect, CDO_CLASS_TABLE_NAME));
         schema.put(GLOBAL_ID_TABLE_NAME, globalIdTableSchema(dialect, GLOBAL_ID_TABLE_NAME));
-   //     schema.put(COMMIT_TABLE_NAME,    commitTableSchema(dialect, COMMIT_TABLE_NAME));
-    //    schema.put(SNAPSHOT_TABLE_NAME,  snapshotTableSchema(dialect, SNAPSHOT_TABLE_NAME));
+        schema.put(COMMIT_TABLE_NAME,    commitTableSchema(dialect, COMMIT_TABLE_NAME));
+        schema.put(SNAPSHOT_TABLE_NAME,  snapshotTableSchema(dialect, SNAPSHOT_TABLE_NAME));
+        schema.put(SNAP_PROPERTY_TABLE_NAME,  snapshotPropertyTableSchema(dialect, SNAP_PROPERTY_TABLE_NAME));
 
         return schema;
     }
