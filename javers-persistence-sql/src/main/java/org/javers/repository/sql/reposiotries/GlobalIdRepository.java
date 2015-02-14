8bf2040a939c5980ff797bda020e194b2b18e8e3
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2015-02-14 12:51:53 +0100

    #104 optimize imports

--- javers-persistence-sql/src/main/java/org/javers/repository/sql/reposiotries/GlobalIdRepository.java
+++ javers-persistence-sql/src/main/java/org/javers/repository/sql/reposiotries/GlobalIdRepository.java
@@ -3,7 +3,6 @@ package org.javers.repository.sql.reposiotries;
 import org.javers.common.collections.Optional;
 import org.javers.core.json.JsonConverter;
 import org.javers.core.metamodel.object.GlobalId;
-import org.javers.repository.sql.schema.FixedSchemaFactory;
 import org.polyjdbc.core.PolyJDBC;
 import org.polyjdbc.core.query.InsertQuery;
 import org.polyjdbc.core.query.SelectQuery;
@@ -12,10 +11,12 @@ import org.polyjdbc.core.query.mapper.ObjectMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.javers.repository.sql.schema.FixedSchemaFactory.*;
+
 public class GlobalIdRepository {
 
-    private static final String NATIVE_QUERY = FixedSchemaFactory.GLOBAL_ID_TABLE_NAME + "." + FixedSchemaFactory.GLOBAL_ID_CLASS_FK + " = " + FixedSchemaFactory.CDO_CLASS_TABLE_NAME + "." + FixedSchemaFactory.CDO_CLASS_PK
-            + " AND " + FixedSchemaFactory.GLOBAL_ID_TABLE_NAME + "." + FixedSchemaFactory.GLOBAL_ID_LOCAL_ID + " = '%s'";
+    private static final String NATIVE_QUERY = GLOBAL_ID_TABLE_NAME + "." + GLOBAL_ID_CLASS_FK + " = " + CDO_CLASS_TABLE_NAME + "." + CDO_CLASS_PK
+            + " AND " + GLOBAL_ID_TABLE_NAME + "." + GLOBAL_ID_LOCAL_ID + " = '%s'";
 
     private PolyJDBC javersPolyjdbc;
     private JsonConverter jsonConverter;
@@ -32,14 +33,14 @@ public class GlobalIdRepository {
 
     private Optional<Long> getIfExists(GlobalId globalId) {
         SelectQuery selectQuery = javersPolyjdbc.query()
-                .select(FixedSchemaFactory.GLOBAL_ID_PK)
-                .from(FixedSchemaFactory.GLOBAL_ID_TABLE_NAME + "," + FixedSchemaFactory.CDO_CLASS_TABLE_NAME)
+                .select(GLOBAL_ID_PK)
+                .from(GLOBAL_ID_TABLE_NAME + "," + CDO_CLASS_TABLE_NAME)
                 .where(String.format(NATIVE_QUERY, jsonConverter.toJson(globalId.getCdoId())));
 
         return Optional.fromNullable(javersPolyjdbc.queryRunner().queryUnique(selectQuery, new ObjectMapper<Long>() {
             @Override
             public Long createObject(ResultSet resultSet) throws SQLException {
-                return resultSet.getLong(FixedSchemaFactory.GLOBAL_ID_PK);
+                return resultSet.getLong(GLOBAL_ID_PK);
             }
         }, false));
     }
@@ -47,18 +48,18 @@ public class GlobalIdRepository {
     private Long insert(GlobalId globalId) {
         InsertQuery insertClassQuery = javersPolyjdbc.query()
                 .insert()
-                .into(FixedSchemaFactory.CDO_CLASS_TABLE_NAME)
-                .value(FixedSchemaFactory.CDO_CLASS_QUALIFIED_NAME, globalId.getCdoClass().getClientsClass().getName())
-                .sequence(FixedSchemaFactory.CDO_CLASS_PK, FixedSchemaFactory.CDO_PK_SEQ_NAME);
+                .into(CDO_CLASS_TABLE_NAME)
+                .value(CDO_CLASS_QUALIFIED_NAME, globalId.getCdoClass().getClientsClass().getName())
+                .sequence(CDO_CLASS_PK, CDO_PK_SEQ_NAME);
 
         long insertedClassId = javersPolyjdbc.queryRunner().insert(insertClassQuery);
 
         InsertQuery insertGlobalIdQuery = javersPolyjdbc.query()
                 .insert()
-                .into(FixedSchemaFactory.GLOBAL_ID_TABLE_NAME)
-                .value(FixedSchemaFactory.GLOBAL_ID_LOCAL_ID, jsonConverter.toJson(globalId.getCdoId()))
-                .value(FixedSchemaFactory.GLOBAL_ID_CLASS_FK, insertedClassId)
-                .sequence(FixedSchemaFactory.GLOBAL_ID_PK, FixedSchemaFactory.GLOBAL_ID_PK_SEQ);
+                .into(GLOBAL_ID_TABLE_NAME)
+                .value(GLOBAL_ID_LOCAL_ID, jsonConverter.toJson(globalId.getCdoId()))
+                .value(GLOBAL_ID_CLASS_FK, insertedClassId)
+                .sequence(GLOBAL_ID_PK, GLOBAL_ID_PK_SEQ);
 
         return javersPolyjdbc.queryRunner().insert(insertGlobalIdQuery);
     }
