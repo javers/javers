4a5680ea641ec1f1c80595cf1c7e1279f4ad6283
bartosz.walacik <bwalacik@gmail.com>
2015-02-19 23:08:48 +0100

    https://github.com/javers/javers/issues/102 added tables cleanup for Postgres

--- javers-persistence-sql/src/test/groovy/org/javers/repository/sql/JaversSqlRepositoryE2ETest.groovy
+++ javers-persistence-sql/src/test/groovy/org/javers/repository/sql/JaversSqlRepositoryE2ETest.groovy
@@ -2,6 +2,7 @@ package org.javers.repository.sql
 
 import org.h2.tools.Server
 import org.javers.core.JaversRepositoryE2ETest
+import spock.lang.Shared
 
 import java.sql.Connection
 import java.sql.DriverManager
@@ -10,6 +11,7 @@ import static org.javers.core.JaversBuilder.javers
 
 class JaversSqlRepositoryE2ETest extends JaversRepositoryE2ETest {
 
+    @Shared
     Connection dbConnection;
     
     @Override
@@ -18,6 +20,7 @@ class JaversSqlRepositoryE2ETest extends JaversRepositoryE2ETest {
         dbConnection = DriverManager.getConnection("jdbc:h2:tcp://localhost:9092/mem:test")
         //dbConnection = DriverManager.getConnection("jdbc:postgresql://horton.elephantsql.com:5432/xzvpycnt", "xzvpycnt", "******");
         //dbConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/javers", "javers", "javers");
+
         def connectionProvider = new ConnectionProvider() {
             @Override
             Connection getConnection() {
@@ -28,9 +31,25 @@ class JaversSqlRepositoryE2ETest extends JaversRepositoryE2ETest {
         def sqlRepository = SqlRepositoryBuilder.sqlRepository().withConnectionProvider(connectionProvider).withDialect(DialectName.POSTGRES).build()
         javers = javers().registerJaversRepository(sqlRepository).build()
         sqlRepository.setJsonConverter(javers.jsonConverter)
+
+        clearTables()
     }
     
     def cleanup() {
         dbConnection.close()
+    }
+
+    def clearTables(){
+        execute("delete  from jv_snapshot_property;")
+        execute("delete  from jv_snapshot;")
+        execute("delete  from jv_commit;")
+        execute("delete  from jv_global_id;")
+        execute("delete  from jv_cdo_class;")
+    }
+
+   def execute(String sql){
+        def stmt = dbConnection.createStatement()
+        stmt.executeUpdate(sql)
+        stmt.close()
     }
 }
