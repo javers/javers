e2d4917c23aae3ac30499fab39909c26ac237193
bartosz.walacik <bwalacik@gmail.com>
2015-02-20 08:24:55 +0100

    https://github.com/javers/javers/issues/102 rollback() instead of tables cleanup

--- javers-persistence-sql/src/test/groovy/org/javers/repository/sql/JaversSqlRepositoryE2ETest.groovy
+++ javers-persistence-sql/src/test/groovy/org/javers/repository/sql/JaversSqlRepositoryE2ETest.groovy
@@ -11,7 +11,6 @@ import static org.javers.core.JaversBuilder.javers
 
 class JaversSqlRepositoryE2ETest extends JaversRepositoryE2ETest {
 
-    @Shared
     Connection dbConnection;
     
     @Override
@@ -19,7 +18,10 @@ class JaversSqlRepositoryE2ETest extends JaversRepositoryE2ETest {
         Server.createTcpServer().start()
         dbConnection = DriverManager.getConnection("jdbc:h2:tcp://localhost:9092/mem:test")
         //dbConnection = DriverManager.getConnection("jdbc:postgresql://horton.elephantsql.com:5432/xzvpycnt", "xzvpycnt", "******");
-        //dbConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/javers", "javers", "javers");
+
+        //PG
+        //dbConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/javers", "javers", "javers")
+        //dbConnection.setAutoCommit(false)
 
         def connectionProvider = new ConnectionProvider() {
             @Override
@@ -28,17 +30,20 @@ class JaversSqlRepositoryE2ETest extends JaversRepositoryE2ETest {
             }
         }
         
-        def sqlRepository = SqlRepositoryBuilder.sqlRepository().withConnectionProvider(connectionProvider).withDialect(DialectName.POSTGRES).build()
+        def sqlRepository = SqlRepositoryBuilder
+                .sqlRepository()
+                .withConnectionProvider(connectionProvider)
+                .withDialect(DialectName.H2).build()
         javers = javers().registerJaversRepository(sqlRepository).build()
         sqlRepository.setJsonConverter(javers.jsonConverter)
-
-        clearTables()
     }
     
     def cleanup() {
+        dbConnection.rollback()
         dbConnection.close()
     }
 
+    /*
     def clearTables(){
         execute("delete  from jv_snapshot_property;")
         execute("delete  from jv_snapshot;")
@@ -47,9 +52,9 @@ class JaversSqlRepositoryE2ETest extends JaversRepositoryE2ETest {
         execute("delete  from jv_cdo_class;")
     }
 
-   def execute(String sql){
+    def execute(String sql){
         def stmt = dbConnection.createStatement()
         stmt.executeUpdate(sql)
         stmt.close()
-    }
+    }*/
 }
