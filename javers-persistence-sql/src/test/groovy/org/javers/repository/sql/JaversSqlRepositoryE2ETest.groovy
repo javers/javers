a8b092779d443773170331e86dfb9ff45987d254
bartosz.walacik <bwalacik@gmail.com>
2015-03-05 23:35:37 +0100

    JaversSqlRepositoryE2ETest based on H2 moved back to main test suite Postgres and MySql integration tests omitted due tue broken configuration

--- javers-persistence-sql/src/test/groovy/org/javers/repository/sql/JaversSqlRepositoryE2ETest.groovy
+++ javers-persistence-sql/src/test/groovy/org/javers/repository/sql/JaversSqlRepositoryE2ETest.groovy
@@ -1,7 +1,73 @@
 package org.javers.repository.sql
 
-/**
- * @author bartosz walacik
- */
-class JaversSqlRepositoryE2ETest {
+import org.h2.tools.Server
+import org.javers.core.JaversRepositoryE2ETest
+import org.javers.core.model.SnapshotEntity
+import org.javers.repository.sql.reposiotries.PersistentGlobalId
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+import static org.javers.core.JaversBuilder.javers
+import static org.javers.core.metamodel.object.InstanceIdDTO.instanceId
+
+class JaversSqlRepositoryE2ETest extends JaversRepositoryE2ETest {
+
+    Connection dbConnection;
+
+    @Override
+    def setup() {
+        Server.createTcpServer().start()
+        dbConnection = DriverManager.getConnection("jdbc:h2:mem:sql_test");//;TRACE_LEVEL_SYSTEM_OUT=2")
+
+        dbConnection.setAutoCommit(false)
+
+        def connectionProvider = { dbConnection } as ConnectionProvider
+
+        def sqlRepository = SqlRepositoryBuilder
+                .sqlRepository()
+                .withConnectionProvider(connectionProvider)
+                .withDialect(DialectName.H2).build()
+        javers = javers().registerJaversRepository(sqlRepository).build()
+    }
+
+    def "should not interfere with user transactions"() {
+        given:
+        def anEntity = new SnapshotEntity(id:1)
+
+        when:
+        javers.commit("author", anEntity)
+        dbConnection.rollback()
+        def snapshots = javers.getStateHistory(instanceId(1, SnapshotEntity), 2)
+
+        then:
+        !snapshots
+
+        when:
+        javers.commit("author", anEntity)
+        dbConnection.commit()
+        snapshots = javers.getStateHistory(instanceId(1, SnapshotEntity), 2)
+
+        then:
+        snapshots.size() == 1
+    }
+
+    def "should preserve globalId.pk as PersistentGlobalId to minimize number of queries"() {
+        given:
+        def anEntity = new SnapshotEntity(id:1)
+        javers.commit("author", anEntity)
+
+        when:
+        anEntity.intProperty = 2
+        def commit = javers.commit("author", anEntity)
+
+        then:
+        commit.snapshots.get(0).globalId instanceof PersistentGlobalId
+        commit.snapshots.get(0).globalId.primaryKey > 0
+    }
+
+    def cleanup() {
+        dbConnection.rollback()
+        dbConnection.close()
+    }
 }
