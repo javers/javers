02999a2065a01ac72cde786d2a75f4704ccd5a5c
bartosz.walacik <bwalacik@gmail.com>
2015-11-04 09:13:10 +0100

    using local mongo in Travis

--- javers-persistence-mongo/src/test/groovy/org/javers/repository/mongo/integration/MongoIntegrationTest.groovy
+++ javers-persistence-mongo/src/test/groovy/org/javers/repository/mongo/integration/MongoIntegrationTest.groovy
@@ -17,11 +17,7 @@ class MongoIntegrationTest extends JaversRepositoryE2ETest {
 
     @Override
     def setup() {
-        def credentials = MongoCredential.createCredential("javers", "j_int_test", "123456".toCharArray())
-        def mongoDb = new MongoClient(
-            new ServerAddress("ds031982.mongolab.com",31982),
-            [credentials]
-        ).getDatabase("j_int_test")
+        def mongoDb = new MongoClient().getDatabase("j_int_test")
 
         def mongoRepository = new MongoRepository(mongoDb)
 
