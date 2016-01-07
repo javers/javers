5755e1a15884f0a70b5bac5b70620a2f84ae297a
bartoszwalacik <bwalacik@gmail.com>
2016-01-07 22:00:03 +0100

    spring-factories

--- javers-spring-boot-starter-mongo/src/test/java/org/javers/spring/boot/mongo/TestApplication.java
+++ javers-spring-boot-starter-mongo/src/test/java/org/javers/spring/boot/mongo/TestApplication.java
@@ -2,12 +2,10 @@ package org.javers.spring.boot.mongo;
 
 import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author pawelszymczyk
@@ -15,7 +13,6 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan("org.javers.spring.boot.mongo")
-@Import(JaversMongoAutoConfiguration.class)
 public class TestApplication {
 
     @Bean
