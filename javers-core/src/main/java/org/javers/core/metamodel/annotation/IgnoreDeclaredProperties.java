fd7d6a9d394d8131290d75a957643691eaad1172
bartoszwalacik <bwalacik@gmail.com>
2016-11-06 18:22:22 +0100

    javadoc

--- javers-core/src/main/java/org/javers/core/metamodel/annotation/IgnoreDeclaredProperties.java
+++ javers-core/src/main/java/org/javers/core/metamodel/annotation/IgnoreDeclaredProperties.java
@@ -7,20 +7,49 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
+ * Use IgnoreDeclaredProperties annotation to mark
+ * all properties <b>declared</b> in a given class as ignored by JaVers.
+ * <br/><br/>
  *
- * Add {@link IgnoreDeclaredProperties} to classes to ignore all their properties.
- * <br>
- * <br>
- * If a class is annotated with {@link IgnoreDeclaredProperties} and is part of an audited hierarchy,
- * only the properties of the class are ignored, and JaVers will still track any other properties in the
- * class hierarchy.
- * By contrast, if a class is annotated with {@link DiffIgnore} JaVers will ignore all instances of
- * that class.
+ * JaVers still tracks instances of a given class and tracks changes done on properties of
+ * its superclass
+ * (by contrast, if a class is annotated with {@link DiffIgnore}, JaVers completely ignores instances of
+ * that class).
+ *
+ * <br/><br/>
+ * For example, when you want to ignore all properties declared in a subclass B but
+ * still track changes in properties declared in a superclass A:
+ * <pre>
+ * class A {
+ *     &#64;Id
+ *     private Long id;
+ *     private String name;
+ * }
+ * </pre>
+ *
+ * this mapping:
+ * <pre>
+ * &#64;IgnoreDeclaredProperties
+ * class B extends A {
+ *     private String foo;
+ *     private String bar;
+ * }
+ * </pre>
+ *
+ * is equivalent to:
+ * <pre>
+ * class B extends A {
+ *     &#64;DiffIgnore
+ *     private String foo;
+ *     &#64;DiffIgnore
+ *     private String bar;
+ * }
+ * </pre>
+ *
  * @see DiffIgnore
  * @author Edward Mallia
  */
 @Target({ TYPE})
 @Retention(RUNTIME)
-public @interface IgnoreDeclaredProperties
-{
+public @interface IgnoreDeclaredProperties {
 }
