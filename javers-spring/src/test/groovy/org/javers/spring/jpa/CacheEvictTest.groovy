ea6368aee38863a93eadebd2aacf4ecaede5b2b7
bartoszwalacik <bwalacik@gmail.com>
2016-11-28 09:01:55 +0100

    https://github.com/javers/javers/issues/467 test fixes

--- javers-spring/src/test/groovy/org/javers/spring/jpa/CacheEvictTest.groovy
+++ javers-spring/src/test/groovy/org/javers/spring/jpa/CacheEvictTest.groovy
@@ -1,14 +1,19 @@
 package org.javers.spring.jpa
 
+import groovy.sql.Sql
 import org.javers.core.Javers
+import org.javers.hibernate.integration.config.HibernateConfig
 import org.javers.hibernate.integration.entity.Person
 import org.javers.hibernate.integration.entity.PersonCrudRepository
 import org.javers.repository.jql.QueryBuilder
+import org.javers.repository.sql.ConnectionProvider
 import org.javers.repository.sql.JaversSqlRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.IntegrationTest
 import org.springframework.boot.test.SpringApplicationConfiguration
 import spock.lang.Specification
+
+import static org.javers.hibernate.integration.config.HibernateConfig.H2_URL
 
 /**
  * @author bartosz.walacik
@@ -28,6 +33,11 @@ class CacheEvictTest extends Specification{
 
     @Autowired
     ErrorThrowingService errorThrowingService
+
+    def setup() {
+        def sql = Sql.newInstance(H2_URL, "org.h2.Driver")
+        sql.execute("DELETE jv_snapshot")
+    }
 
     def "should evict GlobalId PK Cache after rollback"(){
       given:
