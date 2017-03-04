571861fce3558f745aefc51230744aeea9501d54
bartoszwalacik <bwalacik@gmail.com>
2017-03-04 13:04:54 +0100

    signing key

--- travis/deployToSonatype.sh
+++ travis/deployToSonatype.sh
@@ -1,5 +1,5 @@
 echo "uploadArchives..."
-./gradlew uploadArchives -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword -Psigning.keyId=$signingKeyId -Psigning.password=$signingPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/travis/codesigning.asc
+./gradlew uploadArchives -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword -Psigning.keyId=$signingKeyId -Psigning.password=$signingPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/travis/secring.gpg
 
 echo "closeAndPromoteRepository..."
 ./gradlew closeAndPromoteRepository -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword
