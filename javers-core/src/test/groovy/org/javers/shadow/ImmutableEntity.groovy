9687a67871f6b7566c351a9962eadface8c49baa
Bartosz Walacik <bartosz.walacik@allegrogroup.com>
2017-03-20 11:49:05 +0100

    another test

--- javers-core/src/test/groovy/org/javers/shadow/ImmutableEntity.groovy
+++ javers-core/src/test/groovy/org/javers/shadow/ImmutableEntity.groovy
@@ -6,7 +6,18 @@ class ImmutableEntity {
     @Id
     private final int id
 
+    private final ImmutableEntity entityRef
+
+    ImmutableEntity(int id, ImmutableEntity entityRef) {
+        this.id = id
+        this.entityRef = entityRef
+    }
+
     int getId() {
         return id
+    }
+
+    ImmutableEntity getEntityRef() {
+        return entityRef
     }
 }
