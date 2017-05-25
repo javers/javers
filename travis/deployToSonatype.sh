6f953cfb16c66d2dcba8851872b0c6e18c5ff628
Bartosz Walacik <bwalacik@gmail.com>
2017-05-25 09:49:57 +0200

    io.codearte.gradle tuning

--- travis/deployToSonatype.sh
+++ travis/deployToSonatype.sh
@@ -2,4 +2,4 @@ echo "uploadArchives..."
 ./gradlew uploadArchives -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword -Psigning.keyId=$signingKeyId -Psigning.password=$signingPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/travis/secring.gpg
 
 echo "closeAndPromoteRepository..."
-./gradlew closeAndReleaseRepository -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword
+./gradlew closeAndReleaseRepository -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword --info
