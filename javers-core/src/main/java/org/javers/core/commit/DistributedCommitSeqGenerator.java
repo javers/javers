0e1a521ff23418bb07d1c0d0b3e553ca8373332b
Bartosz Walacik <bwalacik@gmail.com>
2017-10-03 22:54:15 +0200

    DistributedCommitSeqGenerator is deprecated

--- javers-core/src/main/java/org/javers/core/commit/DistributedCommitSeqGenerator.java
+++ javers-core/src/main/java/org/javers/core/commit/DistributedCommitSeqGenerator.java
@@ -6,8 +6,10 @@ import java.util.UUID;
  * Generates commitId with random majorId and minorId set to 0.
  * Can be used in distributed applications.
  *
+ * @deprecated
  * @author bartosz.walacik
  */
+@Deprecated
 class DistributedCommitSeqGenerator {
     CommitId nextId() {
         return new CommitId(Math.abs(UUID.randomUUID().getLeastSignificantBits()),0);
