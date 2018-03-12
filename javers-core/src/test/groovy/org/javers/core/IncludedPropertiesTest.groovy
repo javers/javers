d544c9cfc8a1b9d90d25265c3789100100c356fc
Iulian Stefanica <stefanicai@gmail.com>
2018-03-12 13:09:42 +0200

    Updated test cases for IncludedPropertiesTest. Fixed issue by removing unnecessary check in ManagedPropertiesFilter.

--- javers-core/src/test/groovy/org/javers/core/IncludedPropertiesTest.groovy
+++ javers-core/src/test/groovy/org/javers/core/IncludedPropertiesTest.groovy
@@ -1,14 +1,13 @@
 package org.javers.core
 
 import org.javers.core.metamodel.clazz.EntityDefinitionBuilder
-import org.javers.core.model.DummyClassWithCyclicReference
-import org.javers.core.model.DummyClassWithCyclicReference2
+import org.javers.core.metamodel.clazz.JaversEntity
 import org.javers.core.model.DummyUser
 import spock.lang.Specification
 
 class IncludedPropertiesTest extends Specification {
 
-    def "Javers should be able to build when a class is configured using ClientsClassConfiguration with includedProperties"() {
+    def "should be able to commit when a class is configured using ClientsClassConfiguration with includedProperties"() {
         when:
         def javers = JaversBuilder.javers().registerEntity(entityDefinition).build()
         javers.commit("user", entityInstance)
@@ -23,12 +22,20 @@ class IncludedPropertiesTest extends Specification {
 
     def "should handle cyclical objects" () {
         when:
-        def javers = JaversBuilder.javers().registerEntity(EntityDefinitionBuilder.entityDefinition(DummyClassWithCyclicReference.class).withIdPropertyName("name").withIncludedProperties(["name", "child"]).build()).build()
-        def entityInstance = new DummyClassWithCyclicReference(name: "Johny")
+        def javers = JaversBuilder.javers().registerEntity(EntityDefinitionBuilder.entityDefinition(DummyClassWithCyclicReference.class).withIdPropertyName("id").withIncludedProperties(["id", "child"]).build()).build()
+        def entityInstance = new DummyClassWithCyclicReference(id: 1)
         entityInstance.child = new DummyClassWithCyclicReference2(parent: entityInstance);
         javers.commit("user", entityInstance)
 
         then:
         noExceptionThrown()
     }
+}
+
+class DummyClassWithCyclicReference2 {
+    DummyClassWithCyclicReference parent
+}
+
+class DummyClassWithCyclicReference extends JaversEntity {
+    DummyClassWithCyclicReference2 child;
 }
--- javers-core/src/test/groovy/org/javers/core/model/DummyClassWithCyclicReference2.groovy
+++
@@ -1,5 +0,0 @@
-package org.javers.core.model
-
-class DummyClassWithCyclicReference2 {
-    DummyClassWithCyclicReference parent
-}
