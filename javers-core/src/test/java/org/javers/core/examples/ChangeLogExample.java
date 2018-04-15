e534a99864752bcaa91a52cf589cc6de3522a3cb
Bartosz Walacik <bwalacik@gmail.com>
2018-04-15 22:06:29 +0200

    ChangeLogExample

--- javers-core/src/test/java/org/javers/core/examples/ChangeLogExample.java
+++ javers-core/src/test/java/org/javers/core/examples/ChangeLogExample.java
@@ -1,5 +1,6 @@
 package org.javers.core.examples;
 
+import org.javers.core.Changes;
 import org.javers.core.Javers;
 import org.javers.core.JaversBuilder;
 import org.javers.core.changelog.SimpleTextChangeLog;
@@ -10,6 +11,42 @@ import org.junit.Test;
 import java.util.List;
 
 public class ChangeLogExample {
+
+    @Test
+    public void changesPrintingExampleWithGrouping() {
+        // given
+        Javers javers = JaversBuilder.javers().build();
+
+        Employee sam = new Employee("Sam", 1_000);
+        Employee frodo = new Employee("Frodo", 10_000);
+        javers.commit("author", frodo);
+
+        frodo.addSubordinate(sam);
+        frodo.setSalary(11_000);
+        sam.setSalary(2_000);
+        javers.commit("author", frodo);
+
+        // when
+        Changes changes = javers.findChanges(QueryBuilder.byClass(Employee.class)
+                .withNewObjectChanges().build());
+
+        //then
+        System.out.println("Printing the flat list of Changes :");
+        changes.forEach(change -> {
+            System.out.println("- " + change);
+        });
+
+        System.out.println("Printing Changes with grouping by commit and by object :");
+        changes.groupByCommit().forEach(byCommit -> {
+            System.out.println("commit " + byCommit.getCommit().getId());
+            byCommit.groupByObject().forEach(byObject -> {
+                System.out.println("  changes on " + byObject.getGlobalId().value() + " : ");
+                byObject.get().forEach(change -> {
+                    System.out.println("  - " + change);
+                });
+            });
+        });
+    }
 
     @Test
     public void shouldPrintTextChangeLog() {
