14ec8ef7bc17b45654219529d5546f6dc8d52a15
Bartosz Walacik <bwalacik@gmail.com>
2018-10-19 11:38:55 +0200

    https://github.com/javers/javers/issues/593 failing test

---
+++ javers-core/src/test/groovy/org/javers/core/cases/CaseAsyncCommit.groovy
@@ -0,0 +1,42 @@
+package org.javers.core.cases
+
+import org.javers.core.JaversBuilder
+import org.javers.core.model.SnapshotEntity
+import org.javers.repository.jql.QueryBuilder
+import spock.lang.Specification
+
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+
+class CaseAsyncCommit extends Specification {
+
+    def "should commit asynchronously "(){
+      given:
+      def p = new SnapshotEntity(id:1, intProperty:2)
+      def javers = JaversBuilder.javers().build()
+
+      def queue = new ArrayBlockingQueue<>(100)
+      def executor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, queue)
+
+      when:
+      def cf = javers.commitAsync("author", p, [:], executor)
+
+      then:
+      !javers.findSnapshots(QueryBuilder.byInstanceId(1,SnapshotEntity).build())
+
+      when:
+      while(executor.completedTaskCount < 1) {
+          println "executor.completedTaskCount: " + executor.completedTaskCount
+          println "queue.size:                  " + executor.completedTaskCount
+      }
+
+      def snapshot = javers.findSnapshots(QueryBuilder.byInstanceId(1,SnapshotEntity).build()).get(0)
+      def commit = cf.get()
+
+      then:
+      commit.author == "author"
+      commit.snapshots[0].getPropertyValue("intProperty") == 2
+      snapshot.getPropertyValue("intProperty") == 2
+    }
+}
