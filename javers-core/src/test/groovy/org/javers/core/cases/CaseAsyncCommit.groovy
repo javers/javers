61fd8423ea3583efd121974321e959f8c3cab12d
Bartosz Walacik <bwalacik@gmail.com>
2018-10-19 13:21:51 +0200

    https://github.com/javers/javers/issues/593 test fixes

--- javers-core/src/test/groovy/org/javers/core/cases/CaseAsyncCommit.groovy
+++ javers-core/src/test/groovy/org/javers/core/cases/CaseAsyncCommit.groovy
@@ -3,21 +3,29 @@ package org.javers.core.cases
 import org.javers.core.JaversBuilder
 import org.javers.core.model.SnapshotEntity
 import org.javers.repository.jql.QueryBuilder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import spock.lang.Specification
 
 import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
 
 class CaseAsyncCommit extends Specification {
+    private static final Logger logger = LoggerFactory.getLogger(CaseAsyncCommit)
 
     def "should commit asynchronously "(){
       given:
       def p = new SnapshotEntity(id:1, intProperty:2)
       def javers = JaversBuilder.javers().build()
 
-      def queue = new ArrayBlockingQueue<>(100)
-      def executor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, queue)
+      def goFlag = new AtomicBoolean(false)
+      def executor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(100))
+
+      executor.submit({
+          while(goFlag.get()) {}
+      })
 
       when:
       def cf = javers.commitAsync("author", p, [:], executor)
@@ -26,10 +34,9 @@ class CaseAsyncCommit extends Specification {
       !javers.findSnapshots(QueryBuilder.byInstanceId(1,SnapshotEntity).build())
 
       when:
-      while(executor.completedTaskCount < 2) {
-          println "executor.completedTaskCount: " + executor.completedTaskCount
-          println "queue.size:                  " + executor.completedTaskCount
-      }
+      goFlag.set(true)
+      logger.info "waiting for future competition ...."
+      while(executor.completedTaskCount < 3) {}
 
       def snapshot = javers.findSnapshots(QueryBuilder.byInstanceId(1,SnapshotEntity).build()).get(0)
       def commit = cf.get()
