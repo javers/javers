ee9d9ca06c94545919d1d3db04ed64aa9c779bd3
Bartosz Walacik <bwalacik@gmail.com>
2018-10-28 19:35:11 +0100

    merge with master

--- javers-core/src/main/java/org/javers/core/snapshot/SnapshotGraphFactory.java
+++ javers-core/src/main/java/org/javers/core/snapshot/SnapshotGraphFactory.java
@@ -21,12 +21,10 @@ public class SnapshotGraphFactory {
     public SnapshotGraph createLatest(Set<GlobalId> globalIds){
         Validate.argumentIsNotNull(globalIds);
 
-        Set<ObjectNode> snapshotNodes = globalIds.stream()
-            .map(javersRepository::getLatest)
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .map(ObjectNode::new)
-            .collect(Collectors.toSet());
+        Set<ObjectNode> snapshotNodes = javersRepository.getLatest(globalIds)
+                .stream()
+                .map(ObjectNode::new)
+                .collect(Collectors.toSet());
 
         return new SnapshotGraph(snapshotNodes);
     }
