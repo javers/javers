6103874a0e3ae85cba9c8fdd7a0903d98f57aaa6
Bartosz Walacik <bwalacik@gmail.com>
2019-01-05 08:20:21 +0100

    travis deploy disabled

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 ./gradlew -v
 ./gradlew build
 ./gradlew uploadArchives --info
-./gradlew closeAndReleaseRepository :--info
+./gradlew closeAndReleaseRepository --info
