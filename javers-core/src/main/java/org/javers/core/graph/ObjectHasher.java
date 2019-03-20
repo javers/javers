cd648c4534b1cd3051e669c17de8689185aff987
Bartosz Walacik <bwalacik@gmail.com>
2019-03-20 08:35:27 +0100

    https://github.com/javers/javers/issues/810 issue reproduction

--- javers-core/src/main/java/org/javers/core/graph/ObjectHasher.java
+++ javers-core/src/main/java/org/javers/core/graph/ObjectHasher.java
@@ -24,6 +24,9 @@ class ObjectHasher {
                         .map(state -> jsonConverter.toJson(state))
                         .collect(Collectors.joining( "\n" ));
 
+
+        //TODO remove
+        System.out.println(": " + jsonState);
         return ShaDigest.longDigest(jsonState);
     }
 }
