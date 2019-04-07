2b72213a945a8358f06cb0d469c084f60402ca46
bartosz.walacik <bartosz.walacik@allegro.pl>
2019-04-07 20:09:42 +0200

    https://github.com/javers/javers/issues/821 cleanup

--- javers-core/src/main/java/org/javers/core/metamodel/type/ClassType.java
+++ javers-core/src/main/java/org/javers/core/metamodel/type/ClassType.java
@@ -1,7 +1,6 @@
 package org.javers.core.metamodel.type;
 
 import org.javers.common.reflection.ReflectionUtil;
-import org.javers.common.string.ToStringBuilder;
 import org.javers.common.validation.Validate;
 import java.lang.reflect.Type;
 import java.util.Optional;
