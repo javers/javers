3ebc89bea37b5786dcc57bb51aac31073b4b8797
Bartosz Walacik <bwalacik@gmail.com>
2019-07-14 13:37:57 +0200

    https://github.com/javers/javers/issues/732 test clarification

--- javers-core/src/test/groovy/org/javers/core/cases/ValueObjectRefChangeCase.groovy
+++ javers-core/src/test/groovy/org/javers/core/cases/ValueObjectRefChangeCase.groovy
@@ -1,10 +1,8 @@
 package org.javers.core.cases
 
 import org.javers.core.JaversBuilder
-import org.javers.core.diff.changetype.NewObject
 import org.javers.core.diff.changetype.ValueChange
 import org.javers.core.metamodel.annotation.Id
-import org.javers.core.metamodel.object.ValueObjectId
 import spock.lang.Specification
 
 class ValueObjectRefChangeCase extends Specification {
@@ -24,19 +22,18 @@ class ValueObjectRefChangeCase extends Specification {
         def javers = JaversBuilder.javers().build()
 
         when:
-        def diff = javers.compare(new Employee(id: "1"),
+        def diff = javers.compare(new Employee(id: "1", address:null),
                                   new Employee(id: "1", address:new Address(city: "Berlin", street: "x")))
 
-        diff.changes.forEach{println it.toString() + " at:" + it.affectedGlobalId}
+        println diff.prettyPrint()
 
         then:
         diff.changes.size() == 2
 
-        diff.getChangesByType(NewObject).size() == 1
-        def valueChanges = diff.getChangesByType(ValueChange)
-        valueChanges.size() == 1
-        valueChanges[0].left == null
-        valueChanges[0].right instanceof ValueObjectId
-        valueChanges[0].right.value().endsWith('Case$Employee/1#address')
+        diff.changes.forEach {
+            assert it instanceof ValueChange
+            assert it.left == null
+            assert it.right != null
+        }
     }
 }
