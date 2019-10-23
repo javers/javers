934e0f029a52f28392c4907b9c8a4d4d43e0ed12
Bartosz Walacik <bwalacik@gmail.com>
2019-10-23 07:55:23 +0200

    removed wrong @Deprecated ann on DistributedCommitSeqGenerator

--- javers-core/src/main/java/org/javers/core/commit/DistributedCommitSeqGenerator.java
+++ javers-core/src/main/java/org/javers/core/commit/DistributedCommitSeqGenerator.java
@@ -5,10 +5,8 @@ import java.util.UUID;
 /**
  * Generates commitId with random majorId and minorId set to 0.
  *
- *  @deprecated
  * @author bartosz.walacik
  */
-@Deprecated
 class DistributedCommitSeqGenerator {
     CommitId nextId() {
         return new CommitId(Math.abs(UUID.randomUUID().getLeastSignificantBits()),0);
