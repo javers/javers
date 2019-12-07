aa3a472551e50e3a817b2aa91991f52f20b8b189
divikrckr <30746311+divikrckr@users.noreply.github.com>
2019-12-07 15:43:02 +0530

    Create Owners.java

---
+++ javers-core/src/test/java/org/javers/core/examples/model/Owners.java
@@ -0,0 +1,17 @@
+package org.javers.core.examples.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Owners extends ArrayList<Owner> implements Serializable {
+
+	/**
+	 * @author divikrckr
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Owners() {
+		super();
+	}
+	
+}
