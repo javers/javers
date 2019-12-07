e8cd8ed45b10a35bf7310af79c096aab31d04157
divikrckr <30746311+divikrckr@users.noreply.github.com>
2019-12-07 16:15:35 +0530

    Update JaversTest.java

--- javers-core/src/test/java/org/javers/core/examples/JaversTest.java
+++ javers-core/src/test/java/org/javers/core/examples/JaversTest.java
@@ -1,7 +1,16 @@
 package org.javers.core.examples.JaversTest
 
+import static org.junit.Assert.assertEquals;
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.core.diff.Diff;
+import org.javers.core.diff.ListCompareAlgorithm;
+import org.javers.core.diff.changetype.ValueChange;
+import org.junit.Test;
+
 public class JaversTest {
 	private static final ObjectMapper MAPPER = new ObjectMapper();
+	Javers javersInstance =JaversBuilder.javers().withListCompareAlgorithm(ListCompareAlgorithm.LEVENSHTEIN_DISTANCE).build();
 	
 	@Test
 	public void testListAndNodes() {
@@ -9,7 +18,7 @@ public class JaversTest {
 	/*
 	 * Comparing two Json Nodes	
 	 */
-	Diff diff = JaversBuilder.javers().withListCompareAlgorithm(ListCompareAlgorithm.LEVENSHTEIN_DISTANCE).build().compare(
+	Diff diff = javersInstance.compare(
 				MAPPER.valueToTree(new Owner(1,new Addresss("Delhi",null,new CodeInfo(null)))),
 				MAPPER.valueToTree(new Owner(2,new Addresss("Noida","SEZ",new CodeInfo("abc"))))
 				);
@@ -18,7 +27,7 @@ public class JaversTest {
     
     
     /* Comparing two objects */
-    Diff diff1 =JaversInstance.getInstance().compare(
+    Diff diff1 = javersInstance.compare(
 			new Owner(1,new Addresss("Delhi",null,new CodeInfo(null))),
 			new Owner(2,new Addresss("Noida","SEZ",new CodeInfo("abc")))
 			);
@@ -33,9 +42,9 @@ public class JaversTest {
     Owners2.add(new Owner(2,new Addresss("Noida","SEZ",new CodeInfo("abc"))));
     
     
-	Diff diff2 = JaversInstance.getInstance().compare(Owners1, Owners2);
+	Diff diff2 = javersInstance.compare(Owners1, Owners2);
 	
-	Diff diff3 = JaversInstance.getInstance().compare(MAPPER.valueToTree(Owners1),MAPPER.valueToTree (Owners2));
+	Diff diff3 = javersInstance.compare(MAPPER.valueToTree(Owners1),MAPPER.valueToTree (Owners2));
 	
 	assertEquals(diff2.getChangesByType(ValueChange.class).size(), 4);
 	assertEquals(diff3.getChangesByType(ValueChange.class).size(), 4);
