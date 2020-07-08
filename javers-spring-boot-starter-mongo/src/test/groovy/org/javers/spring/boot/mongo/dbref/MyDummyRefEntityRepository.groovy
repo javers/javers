44a40207b5db2f035ad635aae075a2f0be8fdc9d
Bartosz Walacik <bwalacik@gmail.com>
2020-07-08 22:29:22 +0200

    https://github.com/javers/javers/issues/958

--- javers-spring-boot-starter-mongo/src/test/groovy/org/javers/spring/boot/mongo/dbref/MyDummyRefEntityRepository.groovy
+++ javers-spring-boot-starter-mongo/src/test/groovy/org/javers/spring/boot/mongo/dbref/MyDummyRefEntityRepository.groovy
@@ -1,8 +1,6 @@
 package org.javers.spring.boot.mongo.dbref
 
-import org.javers.spring.annotation.JaversSpringDataAuditable
 import org.springframework.data.repository.CrudRepository
 
-@JaversSpringDataAuditable
 interface MyDummyRefEntityRepository extends CrudRepository<MyDummyRefEntity, String> {
 }
