1ddd784b41456cd05b478ca3d478db32fd4ac365
Gustavo <gdsf@cesar.org.br>
2021-02-07 22:02:39 -0300

    feat: add column name to DBNameProvider

--- javers-persistence-sql/src/main/java/org/javers/repository/sql/SqlRepositoryConfiguration.java
+++ javers-persistence-sql/src/main/java/org/javers/repository/sql/SqlRepositoryConfiguration.java
@@ -16,7 +16,31 @@ public class SqlRepositoryConfiguration {
     private final String commitTableName;
     private final String snapshotTableName;
     private final String commitPropertyTableName;
+    
+    private final String globalIdPKColunmName;
+    private final String globalIdLocalIdColumnName;
+    private final String globalIdFragmentColumnName;
+    private final String globalIdTypeNameColumnName;
+    private final String globalIdOwnerIDFKColumnName;
 
+    private final String commitPKColumnName;
+    private final String commitAuthorColumnName;
+    private final String commitCommitDateColumnName;
+    private final String commitCommitDateInstantColumnName;
+    private final String commitCommitIdColumName;
+    private final String commitPropertyCommitFKColumnName;
+    private final String commitPropertyNameColumnName;
+    private final String commitPropertyValueColumnName;
+    
+    private final String snapshotPKColumnName;
+    private final String snapshotCommitFKColumnName;
+    private final String snapshotGlobalIdFKColumnName;
+    private final String snapshotTypeColumnName;
+    private final String snapshotVersionColumnName;
+    private final String snapshotStateColumnName;
+    private final String snapshotChangedColumnName;
+    private final String snapshotManagedTypeColumnName;
+    
     SqlRepositoryConfiguration(boolean globalIdCacheDisabled, String schemaName,
                                       boolean schemaManagementEnabled, String globalIdTableName,
                                       String commitTableName,
@@ -30,6 +54,29 @@ public class SqlRepositoryConfiguration {
         this.commitTableName = commitTableName;
         this.snapshotTableName = snapshotTableName;
         this.commitPropertyTableName = commitPropertyTableName;
+        this.globalIdPKColunmName = null;
+        this.globalIdLocalIdColumnName = null;
+        this.globalIdFragmentColumnName = null;
+        this.globalIdTypeNameColumnName = null;
+        this.globalIdOwnerIDFKColumnName = null;
+        
+        this.commitPKColumnName = null;
+        this.commitAuthorColumnName = null;
+        this.commitCommitDateColumnName = null;
+        this.commitCommitDateInstantColumnName = null;
+        this.commitCommitIdColumName = null;
+        this.commitPropertyCommitFKColumnName = null;
+        this.commitPropertyNameColumnName = null;
+        this.commitPropertyValueColumnName = null;
+        
+        this.snapshotPKColumnName = null;
+        this.snapshotCommitFKColumnName = null;
+        this.snapshotGlobalIdFKColumnName = null;
+        this.snapshotTypeColumnName = null;
+        this.snapshotVersionColumnName = null;
+        this.snapshotStateColumnName = null;
+        this.snapshotChangedColumnName = null;
+        this.snapshotManagedTypeColumnName = null;
     }
 
     public boolean isGlobalIdCacheDisabled() {
@@ -66,4 +113,88 @@ public class SqlRepositoryConfiguration {
     public Optional<String> getCommitPropertyTableName() {
         return Optional.ofNullable(commitPropertyTableName);
     }
+
+	public Optional<String> getGlobalIdPKColunmName() {
+		return Optional.ofNullable(globalIdPKColunmName);
+	}
+
+	public Optional<String> getGlobalIdLocalIdColumnName() {
+		return Optional.ofNullable(globalIdLocalIdColumnName);
+	}
+
+	public Optional<String> getGlobalIdFragmentColumnName() {
+		return Optional.ofNullable(globalIdFragmentColumnName);
+	}
+
+	public Optional<String> getGlobalIdTypeNameColumnName() {
+		return Optional.ofNullable(globalIdTypeNameColumnName);
+	}
+
+	public Optional<String> getGlobalIdOwnerIDFKColumnName() {
+		return Optional.ofNullable(globalIdOwnerIDFKColumnName);
+	}
+
+	public Optional<String> getCommitPKColumnName() {
+		return Optional.ofNullable(commitPKColumnName);
+	}
+
+	public Optional<String> getCommitAuthorColumnName() {
+		return Optional.ofNullable(commitAuthorColumnName);
+	}
+
+	public Optional<String> getCommitCommitDateColumnName() {
+		return Optional.ofNullable(commitCommitDateColumnName);
+	}
+
+	public Optional<String> getCommitCommitDateInstantColumnName() {
+		return Optional.ofNullable(commitCommitDateInstantColumnName);
+	}
+
+	public Optional<String> getCommitCommitIdColumName() {
+		return Optional.ofNullable(commitCommitIdColumName);
+	}
+
+	public Optional<String> getCommitPropertyCommitFKColumnName() {
+		return Optional.ofNullable(commitPropertyCommitFKColumnName);
+	}
+
+	public Optional<String> getCommitPropertyNameColumnName() {
+		return Optional.ofNullable(commitPropertyNameColumnName);
+	}
+
+	public Optional<String> getCommitPropertyValueColumnName() {
+		return Optional.ofNullable(commitPropertyValueColumnName);
+	}
+
+	public Optional<String> getSnapshotPKColumnName() {
+		return Optional.ofNullable(snapshotPKColumnName);
+	}
+
+	public Optional<String> getSnapshotCommitFKColumnName() {
+		return Optional.ofNullable(snapshotCommitFKColumnName);
+	}
+
+	public Optional<String> getSnapshotGlobalIdFKColumnName() {
+		return Optional.ofNullable(snapshotGlobalIdFKColumnName);
+	}
+
+	public Optional<String> getSnapshotTypeColumnName() {
+		return Optional.ofNullable(snapshotTypeColumnName);
+	}
+
+	public Optional<String> getSnapshotVersionColumnName() {
+		return Optional.ofNullable(snapshotVersionColumnName);
+	}
+
+	public Optional<String> getSnapshotStateColumnName() {
+		return Optional.ofNullable(snapshotStateColumnName);
+	}
+
+	public Optional<String> getSnapshotChangedColumnName() {
+		return Optional.ofNullable(snapshotChangedColumnName);
+	}
+
+	public Optional<String> getSnapshotManagedTypeColumnName() {
+		return Optional.ofNullable(snapshotManagedTypeColumnName);
+	}
 }
--- javers-persistence-sql/src/main/java/org/javers/repository/sql/schema/DBNameProvider.java
+++ javers-persistence-sql/src/main/java/org/javers/repository/sql/schema/DBNameProvider.java
@@ -19,6 +19,33 @@ public class DBNameProvider {
     private static final String DEFAULT_COMMIT_TABLE_NAME =    "jv_commit";
     private static final String DEFAULT_COMMIT_PROPERTY_TABLE_NAME = "jv_commit_property";
 
+    public static final String GLOBAL_ID_PK =         "global_id_pk";
+    public static final String GLOBAL_ID_LOCAL_ID =   "local_id";
+    public static final String GLOBAL_ID_FRAGMENT =   "fragment";     //since 1.2
+    public static final String GLOBAL_ID_TYPE_NAME =  "type_name";    //since 2.0
+    public static final String GLOBAL_ID_OWNER_ID_FK ="owner_id_fk";  //since 1.2
+
+    public static final String COMMIT_PK =            "commit_pk";
+    public static final String COMMIT_AUTHOR =        "author";
+    public static final String COMMIT_COMMIT_DATE =   "commit_date";
+    public static final String COMMIT_COMMIT_DATE_INSTANT =   "commit_date_instant";
+    public static final String COMMIT_COMMIT_ID =     "commit_id";
+    public static final String COMMIT_PROPERTY_COMMIT_FK =  "commit_fk";
+    public static final String COMMIT_PROPERTY_NAME =       "property_name";
+    public static final String COMMIT_PROPERTY_VALUE =      "property_value";
+
+    public static final String SNAPSHOT_PK =           "snapshot_pk";
+    public static final String SNAPSHOT_COMMIT_FK =    "commit_fk";
+    public static final String SNAPSHOT_GLOBAL_ID_FK = "global_id_fk";
+    public static final String SNAPSHOT_TYPE =         "type";
+    public static final String SNAPSHOT_VERSION =      "version";
+    public static final String SNAPSHOT_STATE =        "state";
+    public static final String SNAPSHOT_CHANGED =      "changed_properties"; //since v 1.2
+    public static final String SNAPSHOT_MANAGED_TYPE = "managed_type";       //since 2.0
+
+    
+
+    
     private static final Logger logger = LoggerFactory.getLogger(DBNameProvider.class);
     private final SqlRepositoryConfiguration configuration;
 
@@ -85,8 +112,92 @@ public class DBNameProvider {
     Optional<String> getSchemaName() {
         return configuration.getSchemaNameAsOptional();
     }
-
+    
     private DBObjectName fullDbName(String name) {
         return new DBObjectName(configuration.getSchemaNameAsOptional(), name);
+    }
+    
+    String getGlobalIdPKColunmName() {
+        return configuration.getGlobalIdPKColunmName().orElse(GLOBAL_ID_PK);
+    }
+    
+    String getGlobalIdLocalIdColumnName() {
+        return configuration.getGlobalIdLocalIdColumnName().orElse(GLOBAL_ID_LOCAL_ID);
+    }
+
+    String getGlobalIdFragmentColumnName() {
+        return configuration.getGlobalIdFragmentColumnName().orElse(GLOBAL_ID_FRAGMENT);
+    }
+    
+    String getGlobalIdTypeNameColumnName() {
+        return configuration.getGlobalIdTypeNameColumnName().orElse(GLOBAL_ID_TYPE_NAME);
+    }
+    
+    String getGlobalIdOwnerIDFKColumnName() {
+        return configuration.getGlobalIdOwnerIDFKColumnName().orElse(GLOBAL_ID_OWNER_ID_FK);
+    }
+    
+    String getCommitPKColumnName() {
+        return configuration.getCommitPKColumnName().orElse(COMMIT_PK);
+    }
+
+    String getCommitAuthorColumnName() {
+        return configuration.getCommitAuthorColumnName().orElse(COMMIT_AUTHOR);
+    }
+    
+    String getCommitCommitDateColumnName() {
+        return configuration.getCommitCommitDateColumnName().orElse(COMMIT_COMMIT_DATE);
+    }
+    
+    String getCommitCommitDateInstantColumnName() {
+        return configuration.getCommitCommitDateInstantColumnName().orElse(COMMIT_COMMIT_DATE_INSTANT);
+    }
+    
+    String getCommitCommitIdColumName() {
+        return configuration.getCommitCommitIdColumName().orElse(COMMIT_COMMIT_ID);
+    }
+    
+    String getCommitPropertyCommitFKColumnName() {
+        return configuration.getCommitPropertyCommitFKColumnName().orElse(COMMIT_PROPERTY_COMMIT_FK);
+    }
+    
+    String getCommitPropertyNameColumnName() {
+        return configuration.getCommitPropertyNameColumnName().orElse(COMMIT_PROPERTY_NAME);
+    }
+
+    String getCommitPropertyValueColumnName() {
+        return configuration.getCommitPropertyValueColumnName().orElse(COMMIT_PROPERTY_VALUE);
+    }    
+    
+    String getSnapshotPKColumnName() {
+        return configuration.getSnapshotPKColumnName().orElse(SNAPSHOT_PK);
+    }
+
+    String getSnapshotCommitFKColumnName() {
+        return configuration.getSnapshotCommitFKColumnName().orElse(SNAPSHOT_COMMIT_FK);
+    }
+    
+    String getSnapshotGlobalIdFKColumnName() {
+        return configuration.getSnapshotGlobalIdFKColumnName().orElse(SNAPSHOT_GLOBAL_ID_FK);
+    }
+    
+    String getSnapshotTypeColumnName() {
+        return configuration.getSnapshotTypeColumnName().orElse(SNAPSHOT_TYPE);
+    }
+    
+    String getSnapshotVersionColumnName() {
+        return configuration.getSnapshotVersionColumnName().orElse(SNAPSHOT_VERSION);
+    }
+
+    String getSnapshotStateColumnName() {
+        return configuration.getSnapshotStateColumnName().orElse(SNAPSHOT_STATE);
+    }
+    
+    String getSnapshotChangedColumnName() {
+        return configuration.getSnapshotChangedColumnName().orElse(SNAPSHOT_CHANGED);
+    }
+    
+    String getSnapshotManagedTypeColumnName() {
+        return configuration.getSnapshotManagedTypeColumnName().orElse(SNAPSHOT_MANAGED_TYPE);
     }
 }
