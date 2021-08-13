99bc95c09003c636084273511294fb56d8a0f50f
Szymon Kudzia <skudzia@contractors.novantas.com>
2021-08-13 06:33:06 -0400

    Update test

--- javers-persistence-sql/src/test/groovy/org/javers/repository/sql/ListItemRemovalAndReadditionTest.groovy
+++ javers-persistence-sql/src/test/groovy/org/javers/repository/sql/ListItemRemovalAndReadditionTest.groovy
@@ -3,6 +3,8 @@ package org.javers.repository.sql
 import org.javers.core.JaversBuilder
 import org.javers.core.metamodel.annotation.Id
 import org.javers.repository.jql.QueryBuilder
+import org.javers.core.diff.changetype.Atomic
+import org.javers.core.diff.changetype.InitialValueChange
 
 import java.sql.Connection
 import java.sql.DriverManager
@@ -65,60 +67,18 @@ class ListItemRemovalAndReadditionTest extends JaversSqlRepositoryE2ETest {
         javers.commit("author", entity)
 
         when:
-        def changes = javers.findChanges(QueryBuilder.byInstanceId(1, Entity)
+        def commits = javers.findChanges(QueryBuilder.byInstanceId(1, Entity)
             .withChildValueObjects()
             .withScopeDeepPlus()
             .build())
+            .groupByCommit()
 
         then:
-        def actual = changes.prettyPrint()
-            .replaceAll("\\d{2} [a-zA-Z]{3} \\d{4}, (\\d{2}:?){3}", "02 Aug 2021, 00:00:00")
+        commits[0].changes.size() == 2
+        def changes = new ArrayList(commits[0].changes).sort { it.class.name }
 
-        actual == '''\
-        Changes:
-        Commit 4.00 done by author at 02 Aug 2021, 00:00:00 :
-        * changes on org.javers.repository.sql.ListItemRemovalAndReadditionTest$Entity/1 :
-          - 'values' collection changes :
-             0. '...ListItemRemovalAndReadditionTest$Entity/1#values/0' added
-          - 'values/0.name' = 'name'
-        Commit 3.00 done by author at 02 Aug 2021, 00:00:00 :
-        * changes on org.javers.repository.sql.ListItemRemovalAndReadditionTest$Entity/1 :
-          - 'values' collection changes :
-             0. '...ListItemRemovalAndReadditionTest$Entity/1#values/0' removed
-        Commit 2.00 done by author at 02 Aug 2021, 00:00:00 :
-        * changes on org.javers.repository.sql.ListItemRemovalAndReadditionTest$Entity/1 :
-          - 'values' collection changes :
-             0. '...ListItemRemovalAndReadditionTest$Entity/1#values/0' added
-          - 'values/0.name' = 'name'
-        Commit 1.00 done by author at 02 Aug 2021, 00:00:00 :
-        * new object: org.javers.repository.sql.ListItemRemovalAndReadditionTest$Entity/1
-        '''.stripIndent()
-
-//        Use this assertion to see passing test
-//        As you can see Commit 4 has info only about some item being added to the list but zero
-//        info about what actually was added in opposition to Commit 2.
-//        (if class Value had another property and value of this property was changed to something else
-//        before re-adding item to the list then this change would be visible in Commit 4,
-//        this suggests that diff is performed between Commit 4.00 and Commit 2.00 and removal at the Commit 3.00
-//        is simply ignored)
-//
-//        actual == '''\
-//        Changes:
-//        Commit 4.00 done by author at 02 Aug 2021, 00:00:00 :
-//        * changes on org.javers.repository.sql.ListItemRemovalAndReadditionTest$Entity/1 :
-//          - 'values' collection changes :
-//             0. '...ListItemRemovalAndReadditionTest$Entity/1#values/0' added
-//        Commit 3.00 done by author at 02 Aug 2021, 00:00:00 :
-//        * changes on org.javers.repository.sql.ListItemRemovalAndReadditionTest$Entity/1 :
-//          - 'values' collection changes :
-//             0. '...ListItemRemovalAndReadditionTest$Entity/1#values/0' removed
-//        Commit 2.00 done by author at 02 Aug 2021, 00:00:00 :
-//        * changes on org.javers.repository.sql.ListItemRemovalAndReadditionTest$Entity/1 :
-//          - 'values' collection changes :
-//             0. '...ListItemRemovalAndReadditionTest$Entity/1#values/0' added
-//          - 'values/0.name' = 'name'
-//        Commit 1.00 done by author at 02 Aug 2021, 00:00:00 :
-//        * new object: org.javers.repository.sql.ListItemRemovalAndReadditionTest$Entity/1
-//        '''.stripIndent()
+        changes[0] instanceof InitialValueChange
+        changes[0].left == null
+        changes[0].right == "name"
     }
 }
