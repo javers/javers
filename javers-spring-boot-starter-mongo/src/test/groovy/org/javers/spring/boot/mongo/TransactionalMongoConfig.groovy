eee0d007ada80d91aa130be41fa93eca48b462e3
Bartosz Walacik <bwalacik@gmail.com>
2021-11-12 21:59:59 +0100

    https://github.com/javers/javers/issues/1137 fixes

--- javers-spring-boot-starter-mongo/src/test/groovy/org/javers/spring/boot/mongo/TransactionalMongoConfig.groovy
+++ javers-spring-boot-starter-mongo/src/test/groovy/org/javers/spring/boot/mongo/TransactionalMongoConfig.groovy
@@ -1,7 +1,11 @@
 package org.javers.spring.boot.mongo
 
 import com.github.silaev.mongodb.replicaset.MongoDbReplicaSet
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.WriteConcern
 import com.mongodb.client.MongoClient
+import org.jetbrains.annotations.NotNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,6 +13,8 @@ import org.springframework.context.annotation.Profile
 import org.springframework.data.mongodb.MongoDatabaseFactory
 import org.springframework.data.mongodb.MongoTransactionManager
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
+
+import java.util.concurrent.TimeUnit
 
 @Configuration
 @Profile("TransactionalMongo")
@@ -49,5 +55,37 @@ class TransactionalMongoConfig {
         def t = new MongoTransactionManager(mongoDatabaseFactory)
         println("my mongoTransactionManager: "+ t)
         t
+    }
+
+    /**
+     * @author Konstantin Silaev on 3/19/2020
+     */
+    class ConnectionUtils {
+
+        /**
+         * Used for setting timeouts to fail-fast behaviour.
+         */
+        static MongoClientSettings getMongoClientSettingsWithTimeout(
+                final String mongoRsUrlPrimary,
+                final WriteConcern writeConcern,
+                final int timeout
+        ) {
+            final ConnectionString connectionString = new ConnectionString(mongoRsUrlPrimary)
+            return MongoClientSettings.builder()
+                    .writeConcern(writeConcern.withWTimeout(timeout, TimeUnit.SECONDS))
+                    .applyToClusterSettings(c -> c.serverSelectionTimeout(timeout, TimeUnit.SECONDS))
+                    .applyConnectionString(connectionString)
+                    .applyToSocketSettings(
+                            b -> b
+                                    .readTimeout(timeout, TimeUnit.SECONDS)
+                                    .connectTimeout(timeout, TimeUnit.SECONDS)
+                    ).build()
+        }
+
+        static MongoClientSettings getMongoClientSettingsWithTimeout(
+                final String mongoRsUrlPrimary
+        ) {
+            return getMongoClientSettingsWithTimeout(mongoRsUrlPrimary, WriteConcern.W1, 5)
+        }
     }
 }
