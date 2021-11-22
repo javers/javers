f74048d1fe4987c6f25bd2fa012810f2a2920a0f
Bartosz Walacik <bwalacik@gmail.com>
2021-11-22 11:12:36 +0100

    https://github.com/javers/javers/issues/1160

---
+++ javers-core/src/test/groovy/org/javers/core/cases/CaseIllegalStateExceptionWhenGroupByCommit.groovy
@@ -0,0 +1,26 @@
+package org.javers.core.cases
+
+import org.javers.core.JaversBuilder
+import spock.lang.Specification
+
+class MyClass{
+    String value
+}
+
+/**
+ * https://github.com/javers/javers/issues/1160
+ */
+class CaseIllegalStateExceptionWhenGroupByCommit extends Specification {
+
+    def "should groupByCommit" () {
+        given:
+        def javers = JaversBuilder.javers().build()
+
+        when:
+        def diff = javers.compare(new MyClass(value: "abc"), new MyClass(value: "cde"))
+        diff.changes.prettyPrint()
+
+        then:
+        diff.changes
+    }
+}
