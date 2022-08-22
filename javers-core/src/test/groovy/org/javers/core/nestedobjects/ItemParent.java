18ef4e42f8b27e97a22aa2f2eefdcb8f46cee6d5
Christian Kokoska <christian.kokoska@postadress.de>
2022-08-22 19:00:56 +0200

    Test case for bug when comparing nested objects

---
+++ javers-core/src/test/groovy/org/javers/core/nestedobjects/ItemParent.java
@@ -0,0 +1,14 @@
+package org.javers.core.nestedobjects;
+
+public class ItemParent {
+
+    public Item item;
+
+    ItemParent() {
+    }
+
+    ItemParent(Item item) {
+        this.item = item;
+    }
+
+}
---
+++ javers-core/src/test/groovy/org/javers/core/nestedobjects/NestedObjectTest.groovy
@@ -0,0 +1,57 @@
+package org.javers.core.nestedobjects
+
+import org.javers.core.JaversBuilder
+import org.javers.core.diff.changetype.PropertyChange
+import spock.lang.Specification
+
+class NestedObjectTest extends Specification {
+    /*
+    When having two objects of which one has a child object, JaVers is not able to recognize the difference,
+    when the properties of the child object have the same values as the corresponding default value in
+    org.javers.common.collections.Defaults
+
+    The first test ends successful, because the child Item has the value property `1`, which differs from the default
+    in Defaults (0).
+
+    The second test fails, because the child Item has the value property `0`, which is the same as the default in
+    Defaults (0).
+
+    In both tests the itemParentA, which has a child Item is compared to the itemParentB, which has no child Item.
+
+    Please ensure, that removed nested objects are removed properly. I think the Problem is the FakeNode which uses the
+    defaults from Defaults in the methods getDehydratedPropertyValue and getPropertyValue.
+
+    The behaviour can be reproduced with other data types.
+    */
+    def javers = JaversBuilder.javers().build()
+
+    def "should recognize removed nested object a different value then Defaults.int.class"() {
+        given:
+        def item = new Item(1)
+        def itemParentA = new ItemParent(item)
+        def itemParentB = new ItemParent()
+
+        when:
+        def diff = javers.compare(itemParentA, itemParentB)
+
+        println (diff.prettyPrint())
+
+        then:
+        diff.getChangesByType(PropertyChange).size() == 1
+    }
+
+    def "should recognize removed nested object with same value as Defaults.int.class"() {
+        given:
+        def item = new Item(0)
+        def itemParentA = new ItemParent(item)
+        def itemParentB = new ItemParent()
+
+        when:
+        def diff = javers.compare(itemParentA, itemParentB)
+
+        println (diff.prettyPrint())
+
+        then:
+        diff.getChangesByType(PropertyChange).size() == 1
+    }
+}
