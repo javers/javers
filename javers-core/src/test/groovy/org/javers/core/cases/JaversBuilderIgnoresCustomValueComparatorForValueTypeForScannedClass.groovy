97a9dbce9084fdd746d384103ff8ac7085226792
catautobox <catautobox@gmail.com>
2022-10-26 20:05:57 +0200

    Add JaversBuilderIgnoresCustomValueComparatorForValueTypeForScannedClass and JaversBuilderIgnoresCustomValueComparatorWhenRegisteringValueTypeAdapter test cases.

---
+++ javers-core/src/test/groovy/org/javers/core/cases/JaversBuilderIgnoresCustomValueComparatorForValueTypeForScannedClass.groovy
@@ -0,0 +1,34 @@
+package org.javers.core.cases
+
+import org.javers.core.JaversBuilder
+import org.javers.core.diff.custom.BigDecimalComparatorWithFixedEquals
+import org.javers.core.metamodel.type.ManagedType
+import org.javers.core.metamodel.type.ValueType
+import spock.lang.Specification
+
+import static org.javers.core.JaversBuilder.javers
+
+/**
+ * @author catautobox
+ */
+class JaversBuilderIgnoresCustomValueComparatorForValueTypeForScannedClass extends Specification {
+
+    def "should use registered custom value comparator for value type property of scanned class"() {
+        given:
+        JaversBuilder javersBuilder = javers()
+                .registerValue(BigDecimal, new BigDecimalComparatorWithFixedEquals())
+                .scanTypeName(BigDecimalValueHolder)
+
+        when:
+        def javers = javersBuilder.build()
+
+        then:
+        ((ValueType) javers.getTypeMapping(BigDecimal)).hasCustomValueComparator()
+        ((ValueType) ((ManagedType) javers.getTypeMapping(BigDecimalValueHolder)).getProperty("value").getType()).hasCustomValueComparator()
+    }
+
+
+    class BigDecimalValueHolder {
+        BigDecimal value
+    }
+}
