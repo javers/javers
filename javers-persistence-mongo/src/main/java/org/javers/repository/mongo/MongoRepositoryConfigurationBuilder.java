cf331453a22c47119eed57c09db6ce081da7a53a
Elchin Gasimov <egasimov0@gmail.com>
2023-01-17 12:31:56 +0400

    Make HEAD collection name configurable

--- javers-persistence-mongo/src/main/java/org/javers/repository/mongo/MongoRepositoryConfigurationBuilder.java
+++ javers-persistence-mongo/src/main/java/org/javers/repository/mongo/MongoRepositoryConfigurationBuilder.java
@@ -5,6 +5,7 @@ import static org.javers.common.string.Strings.isNonEmpty;
 public class MongoRepositoryConfigurationBuilder {
 
     private String snapshotCollectionName;
+    private String headCollectionName;
     private Integer cacheSize;
     private MongoDialect dialect;
 
@@ -33,7 +34,12 @@ public class MongoRepositoryConfigurationBuilder {
         return this;
     }
 
+    public MongoRepositoryConfigurationBuilder withHeadCollectionName(String headCollectionName) {
+        this.headCollectionName = headCollectionName;
+        return this;
+    }
+
     public MongoRepositoryConfiguration build() {
-        return new MongoRepositoryConfiguration(snapshotCollectionName, cacheSize, dialect);
+        return new MongoRepositoryConfiguration(snapshotCollectionName, headCollectionName, cacheSize, dialect);
     }
 }
