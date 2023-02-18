46881c5536d25d7b1fb18f28b2f36efc4614c061
Bartek Walacik <bwalacik@gmail.com>
2023-02-18 19:28:19 +0100

    javadoc

--- javers-core/src/main/java/org/javers/core/metamodel/annotation/DiffIgnore.java
+++ javers-core/src/main/java/org/javers/core/metamodel/annotation/DiffIgnore.java
@@ -7,7 +7,8 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Use {@code DiffIgnore} annotation to mark certain properties or classes as ignored by JaVers.
+ * Use {@code DiffIgnore} annotation to mark certain properties or classes as ignored by Javers.
+ * <br/><br/>
  *
  * <H2>Property level</H2>
  * Add {@code DiffIgnore} to fields or getters
@@ -15,20 +16,23 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * to mark them as ignored.
  * When used on the property level,
  * {@code DiffIgnore} is equivalent to the {@code javax.persistence.Transient} annotation.
- * <br><br>
- * {@code DiffIgnoreFields} can also be used to ignore multiple properties using one annotation
+ * <br/><br/>
  *
  * <H2>Class level</H2>
  * Add {@code DiffIgnore} to classes to mark them as ignored. <br/>
  * When a class is ignored, all properties
  * (found in other classes) with this class type are ignored.
- * <p>
  * <br/><br/>
  * <b>Warning</b>: {@code DiffIgnore} can't be mixed with {@code DiffInclude} in the same class.
+ * <br/><br/>
+ *
+ * <H2>See also</H2>
+ * To ignore a list of fields given by name -- use {@link DiffIgnoreFields}.
+ * <br/>
+ *
+ * If you want to include only certain fields/methods and ignore others -- use {@link DiffInclude}.
  *
  * @author bartosz walacik
- * @see DiffInclude
- * @see DiffIgnoreFields
  */
 @Target({METHOD, FIELD, TYPE})
 @Retention(RUNTIME)
