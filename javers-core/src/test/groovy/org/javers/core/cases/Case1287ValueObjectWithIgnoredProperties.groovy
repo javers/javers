75f0f4f81c1b7149ce6985e84bb058d534ad2eb7
Kyrylo Merzlikin <kirmerzlikin@gmail.com>
2023-05-13 18:23:37 +0300

    #1287 Fix @DiffIgnoreProperties interference with JaversType resolution

---
+++ javers-core/src/test/groovy/org/javers/core/cases/Case1287ValueObjectWithIgnoredProperties.groovy
@@ -0,0 +1,30 @@
+package org.javers.core.cases
+
+import jakarta.persistence.Id
+import org.javers.core.Javers
+import org.javers.core.JaversBuilder
+import org.javers.core.metamodel.annotation.DiffIgnoreProperties
+import org.javers.core.metamodel.annotation.ValueObject
+import org.javers.core.metamodel.type.ValueObjectType
+import spock.lang.Specification
+
+class Case1287ValueObjectWithIgnoredProperties extends Specification {
+
+	@ValueObject
+	@DiffIgnoreProperties("id")
+	class MyValueObject {
+
+		@Id
+		private UUID id;
+
+		private String property;
+	}
+
+	def "should recognize class as a value object type"() {
+		given:
+		Javers javers = JaversBuilder.javers().build()
+
+		expect:
+		javers.getTypeMapping( MyValueObject ) instanceof ValueObjectType
+	}
+}
