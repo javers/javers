f76e99ecdd546e5021ccae5bb9be8bce28105409
Ida de Groot <ida.degroot@ns.nl>
2024-02-14 16:46:15 +0100

    Add testcase to illustrate issue with object shared between toplevel object and list item.

---
+++ javers-core/src/test/groovy/org/javers/core/cases/CaseSharedObjectUnsetWhenUsedInCollection.groovy
@@ -0,0 +1,50 @@
+package org.javers.core.cases
+
+import groovy.transform.EqualsAndHashCode
+import org.javers.core.JaversBuilder
+import spock.lang.Specification
+
+class CaseSharedObjectUnsetWhenUsedInCollection extends Specification {
+
+    @EqualsAndHashCode
+    class ComplexProperty {
+        String value
+    }
+
+    @EqualsAndHashCode
+    class ListItem {
+        String name
+        ComplexProperty complexProperty
+    }
+
+    @EqualsAndHashCode
+    class TopLevelClass {
+        ComplexProperty complexProperty
+        List<ListItem> items
+    }
+
+    def "should compare shared nested property on fields"() {
+        given:
+        def javers = JaversBuilder.javers().build()
+
+        def l1 = new TopLevelClass(
+                complexProperty: new ComplexProperty(value: "value1"),
+                items: [
+                        new ListItem(name: "name1", complexProperty: new ComplexProperty(value: "value1"))
+                ])
+
+        def complexProperty = new ComplexProperty(value: "value1")
+        def l2 = new TopLevelClass(
+                complexProperty: complexProperty,
+                items: [
+                        new ListItem(name: "name1", complexProperty: complexProperty)
+                ])
+
+        when:
+        def diff = javers.compare(l1, l2)
+
+        then:
+        l1.equals(l2)
+        diff.changes.isEmpty()
+    }
+}
