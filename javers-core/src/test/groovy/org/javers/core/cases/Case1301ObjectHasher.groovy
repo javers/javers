b2104f54f29eaabf8baa197617480362ce07f2bd
Tomas Kupka <tomas.kupka@mgm-tp.com>
2024-03-06 18:38:35 +0100

    fix tests
    https://github.com/javers/javers/issues/1301

--- javers-core/src/test/groovy/org/javers/core/cases/Case1301ObjectHasher.groovy
+++ javers-core/src/test/groovy/org/javers/core/cases/Case1301ObjectHasher.groovy
@@ -2,11 +2,12 @@ package org.javers.core.cases
 
 import org.javers.core.Javers
 import org.javers.core.JaversBuilder
+import org.javers.core.diff.Change
+import org.javers.core.diff.Diff
 import org.javers.core.diff.changetype.PropertyChangeType
 import org.javers.core.diff.changetype.ValueChange
+import org.javers.core.diff.changetype.container.SetChange
 import org.javers.core.graph.HashCodeObjectHasher
-
-import static org.fest.assertions.api.Assertions.assertThat
 
 import groovy.transform.EqualsAndHashCode
 import spock.lang.Specification
@@ -21,16 +22,12 @@ class Case1301ObjectHasher extends Specification {
             Javers javersWithHashCode = JaversBuilder.javers()
                     .registerObjectHasher(HashCodeObjectHasher.class)
                     .build()
-            Javers plainJavers = JaversBuilder.javers()
-                    .build()
-
-
-        when:
             def oldSet = Set.of(new Person("tommy", "Tommy Smart"), new Person("tommy2", "Tommy Smart2"));
             def newSet = Set.of(new Person("tommy2", "Tommy Smart2"), new Person("tommy", "Tommy C. Smart"));
-    
+
+        when:
+
             def diff = javersWithHashCode.compareCollections(oldSet, newSet, Case1301ObjectHasher.Person.class)
-            def plainDiff = plainJavers.compareCollections(oldSet, newSet, Case1301ObjectHasher.Person.class)
 
         then:
             def change = diff.getChangesByType(ValueChange.class).get(0);
@@ -40,12 +37,70 @@ class Case1301ObjectHasher extends Specification {
             change.getPropertyName() == "name"
             change.getLeft() == "Tommy Smart"
             change.getRight() == "Tommy C. Smart"
-            
-            def plainChange = diff.getChangesByType(ValueChange.class).get(0);
-            plainDiff.changes.size() == 5
-
     }
+
+   def "should produce multiple changes when object is identified by JSON"(){
+        given:
+            Javers plainJavers = JaversBuilder.javers()
+                    .build()
+            def oldSet = Set.of(new Person("tommy", "Tommy Smart"), new Person("tommy2", "Tommy Smart2"));
+            def newSet = Set.of(new Person("tommy2", "Tommy Smart2"), new Person("tommy", "Tommy C. Smart"));
+
+
+        when:
     
+            def diff = plainJavers.compareCollections(oldSet, newSet, Case1301ObjectHasher.Person.class)
+            def change1 = findValueChange(diff, "login", true);
+            def change2 = findValueChange(diff, "name", true);
+            def change3 = findValueChange(diff, "login", false);
+            def change4 = findValueChange(diff, "name", false);
+            def changeSet = diff.getChangesByType(SetChange.class).get(0);
+
+        then:
+
+            diff.changes.size() == 5
+
+            change1.getChangeType() == PropertyChangeType.PROPERTY_VALUE_CHANGED
+            change1.getPropertyName() == "login"
+            change1.getLeft() == null
+            change1.getRight() == "tommy"
+            
+            change2.getChangeType() == PropertyChangeType.PROPERTY_VALUE_CHANGED
+            change2.getPropertyName() == "name"
+            change2.getLeft() == null
+            change2.getRight() == "Tommy C. Smart"
+            
+            
+            change3.getChangeType() == PropertyChangeType.PROPERTY_VALUE_CHANGED
+            change3.getPropertyName() == "login"
+            change3.getLeft() == "tommy"
+            change3.getRight() == null
+            
+            
+            change4.getChangeType() == PropertyChangeType.PROPERTY_VALUE_CHANGED
+            change4.getPropertyName() == "name"
+            change4.getLeft() == "Tommy Smart"
+            change4.getRight() == null
+            
+            changeSet.getChangeType() == PropertyChangeType.PROPERTY_VALUE_CHANGED
+            changeSet.getPropertyName() == "set"
+            changeSet.getLeft().size() == 2
+            changeSet.getRight().size() == 2
+    }
+
+	private def findValueChange(Diff diff, String propertyName, boolean leftNull) {
+		List<Change> changes = diff.getChanges{change -> 
+        	if (change instanceof ValueChange) {
+                ValueChange valueChange = (ValueChange) change
+                return valueChange.getPropertyName().equals(propertyName) && Objects.isNull(valueChange.getLeft()) == leftNull
+            }
+            return false
+        };
+        return changes.stream()
+            .findFirst()
+            .orElse(null)
+	}
+
     @EqualsAndHashCode(includes = ["login"], includeFields = true)
     class Person {
         private String login
--- javers-core/src/test/java/org/javers/core/examples/ComparingTopLevelSetVoExample.java
+++ javers-core/src/test/java/org/javers/core/examples/ComparingTopLevelSetVoExample.java
@@ -1,9 +1,13 @@
 package org.javers.core.examples;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.Predicate;
 
 import org.javers.core.Javers;
 import org.javers.core.JaversBuilder;
+import org.javers.core.diff.Change;
 import org.javers.core.diff.Diff;
 import org.javers.core.diff.changetype.PropertyChangeType;
 import org.javers.core.diff.changetype.ValueChange;
@@ -36,27 +40,44 @@ public class ComparingTopLevelSetVoExample {
         assertThat(diff.getChanges()).hasSize(5);
 
         //then
-        //there should be one change of type {@link ValueChange}
+        //there should be four change of type {@link ValueChange}
         ValueChange change = diff.getChangesByType(ValueChange.class).get(0);
-
+        List objectsWithChangedProperty = diff.getObjectsWithChangedProperty("login");
+        Person first = (Person) objectsWithChangedProperty.get(0);
+        Person second = (Person) objectsWithChangedProperty.get(1);
+        //check initial object data
+        assertThat(first.getLogin().equals(second.getLogin()));
+        assertThat(first.equals(second));
+        assertThat(first.hashCode() == second.hashCode());
+        assertThat(!first.getName().equals(second.getName()));
+        
+        objectsWithChangedProperty = diff.getObjectsWithChangedProperty("name");
+        first = (Person) objectsWithChangedProperty.get(0);
+        second = (Person) objectsWithChangedProperty.get(1);
+        assertThat(first.getLogin().equals(second.getLogin()));
+        assertThat(first.equals(second));
+        assertThat(first.hashCode() == second.hashCode());
+        assertThat(!first.getName().equals(second.getName()));
+        
+        change = findValueChange(diff, "login", true);
         assertThat(change.getChangeType()).isEqualTo(PropertyChangeType.PROPERTY_VALUE_CHANGED);
         assertThat(change.getPropertyName()).isEqualTo("login");
         assertThat(change.getLeft()).isNull();
         assertThat(change.getRight()).isEqualTo("tommy");
 
-        change = diff.getChangesByType(ValueChange.class).get(1);
+        change = findValueChange(diff, "name", true);
         assertThat(change.getChangeType()).isEqualTo(PropertyChangeType.PROPERTY_VALUE_CHANGED);
         assertThat(change.getPropertyName()).isEqualTo("name");
         assertThat(change.getLeft()).isNull();
         assertThat(change.getRight()).isEqualTo("Tommy C. Smart");
 
-        change = diff.getChangesByType(ValueChange.class).get(2);
+        change = findValueChange(diff, "login", false);
         assertThat(change.getChangeType()).isEqualTo(PropertyChangeType.PROPERTY_VALUE_CHANGED);
         assertThat(change.getPropertyName()).isEqualTo("login");
         assertThat(change.getRight()).isNull();
         assertThat(change.getLeft()).isEqualTo("tommy");
 
-        change = diff.getChangesByType(ValueChange.class).get(3);
+        change = findValueChange(diff, "name", false);
         assertThat(change.getChangeType()).isEqualTo(PropertyChangeType.PROPERTY_VALUE_CHANGED);
         assertThat(change.getPropertyName()).isEqualTo("name");
         assertThat(change.getRight()).isNull();
@@ -68,6 +89,20 @@ public class ComparingTopLevelSetVoExample {
         assertThat(changeSet.getLeft().size()).isEqualTo(2);
         assertThat(changeSet.getRight().size()).isEqualTo(2);
 
+    }
+    
+    private ValueChange findValueChange(Diff diff, String propertyName, boolean leftNull) {
+        List<Change> changes = diff.getChanges(change -> {
+        	if (change instanceof ValueChange) {
+        		ValueChange valueChange = (ValueChange) change;
+        		return valueChange.getPropertyName().equals(propertyName) && Objects.isNull(valueChange.getLeft()) == leftNull;
+        	}
+        	return false;
+        });
+        return changes.stream()
+        	.map(ValueChange.class::cast)
+        	.findFirst()
+        	.orElse(null);
     }
 
     @Test
