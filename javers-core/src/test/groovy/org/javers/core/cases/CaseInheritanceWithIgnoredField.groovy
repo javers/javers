1a8a5e3b850e6962ba5a3c4d3dbd4715bc85d122
khan <clarkhan@163.com>
2024-06-05 16:44:17 +0800

    test: test case for ignored fields

---
+++ javers-core/src/test/groovy/org/javers/core/cases/CaseInheritanceWithIgnoredField.groovy
@@ -0,0 +1,50 @@
+package org.javers.core.cases
+
+import jakarta.persistence.Id
+import org.javers.core.JaversBuilder
+import org.javers.core.metamodel.annotation.DiffIgnore
+import org.javers.core.metamodel.annotation.TypeName
+import org.javers.core.metamodel.type.EntityType
+import spock.lang.Specification
+
+/**
+ * @author khan
+ */
+class CaseInheritanceWithIgnoredField extends Specification {
+    @TypeName("A")
+    class A {
+        @Id
+        String id
+        String field
+    }
+    @TypeName("B")
+    class B extends A {
+        @DiffIgnore
+        String someOtherField
+    }
+    @TypeName("C")
+    class C extends A {
+        @DiffIgnore
+        String someOtherField
+    }
+
+    def "should ignore fields on sub Class that are annotated with DiffIgnore"(){
+        given:
+        def javers = JaversBuilder.javers().build()
+
+        when:
+        EntityType bType = javers.getTypeMapping(B)
+        // get typeMapping of the parent class
+        EntityType aType = javers.getTypeMapping(A)
+        EntityType cType = javers.getTypeMapping(C)
+
+        then:
+        println aType.prettyPrint()
+        println bType.prettyPrint()
+        println cType.prettyPrint()
+
+        assert bType.findProperty("someOtherField").isEmpty()
+        assert cType.findProperty("someOtherField").isEmpty()
+    }
+
+}
