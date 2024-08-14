b9a1e0d2382ccd2e8b08b5943c5324891be23047
Ivan Simeonov <ivannicksimeonov@gmail.com>
2024-08-14 12:14:16 +0300

    Added support for sentinel pool

--- javers-persistence-redis/src/main/java/org/javers/repository/redis/CdoSnapshotKeyExpireListener.java
+++ javers-persistence-redis/src/main/java/org/javers/repository/redis/CdoSnapshotKeyExpireListener.java
@@ -3,27 +3,28 @@ package org.javers.repository.redis;
 import static org.javers.repository.redis.JaversRedisRepository.JV_SNAPSHOTS;
 import static org.javers.repository.redis.JaversRedisRepository.JV_SNAPSHOTS_ENTITY_KEYS;
 
-import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
+import redis.clients.jedis.util.Pool;
 
 public class CdoSnapshotKeyExpireListener extends JedisPubSub {
 
-    private final JedisPool jedisPool;
+  private final Pool<Jedis> jedisPool;
 
-    public CdoSnapshotKeyExpireListener(final JedisPool jedisPool) {
-        this.jedisPool = jedisPool;
-    }
+  public CdoSnapshotKeyExpireListener(final Pool<Jedis> jedisPool) {
+    this.jedisPool = jedisPool;
+  }
 
-    @Override
-    public void onPMessage(String pattern, String channel, String message) {
-        if ("expired".equals(message)) {
-            final var key = channel.substring(channel.indexOf(JV_SNAPSHOTS));
-            final var entityName = key.substring(13, key.indexOf("/"));
-            try (final var jedis = jedisPool.getResource()) {
-                jedis.srem(JV_SNAPSHOTS_ENTITY_KEYS, key);
-                jedis.srem(JV_SNAPSHOTS_ENTITY_KEYS.concat(":").concat(entityName), key);
-            }
-        }
+  @Override
+  public void onPMessage(String pattern, String channel, String message) {
+    if ("expired".equals(message)) {
+      final var key = channel.substring(channel.indexOf(JV_SNAPSHOTS));
+      final var entityName = key.substring(13, key.indexOf("/"));
+      try (final var jedis = jedisPool.getResource()) {
+        jedis.srem(JV_SNAPSHOTS_ENTITY_KEYS, key);
+        jedis.srem(JV_SNAPSHOTS_ENTITY_KEYS.concat(":").concat(entityName), key);
+      }
     }
+  }
 
 }
