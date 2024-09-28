efbe200a3b38c46177042134969053b6806fc7a5
Brett Brakefield <brett.brakefield@gmail.com>
2024-09-28 17:07:27 -0500

    Adding test case to isolate Double.NaN error

---
+++ javers-spring-boot-starter-mongo/src/test/java/org/javers/spring/boot/mongo/DummyEntityWithDouble.java
@@ -0,0 +1,25 @@
+package org.javers.spring.boot.mongo;
+
+import org.javers.core.metamodel.annotation.Id;
+
+/**
+ * @author pawelszymczyk
+ */
+public class DummyEntityWithDouble {
+
+    @Id
+    private final int id;
+    private final double doubleValue;
+
+    public DummyEntityWithDouble(int id, double doubleValue) {
+        this.id = id;
+        this.doubleValue = doubleValue;
+    }
+
+    @Id
+    public int getId() {
+        return id;
+    }
+
+    public double getDoubleValue() {return doubleValue;}
+}
