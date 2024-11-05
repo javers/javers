03febd9fd356bc555fecf0a77eb7ca07ff4c812a
Andreas Mautsch <andreas.mautsch@cgm.com>
2024-11-05 11:35:07 +0100

    Spring Native Support added

--- javers-spring-boot-starter-sql/src/main/java/org/javers/spring/boot/aot/JaversNativeHintsConfiguration.java
+++ javers-spring-boot-starter-sql/src/main/java/org/javers/spring/boot/aot/JaversNativeHintsConfiguration.java
@@ -9,8 +9,8 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 import java.util.Arrays;
 
 @Configuration
-@ImportRuntimeHints(JaversNativeHints.ApplicationRuntimeHints.class)
-public class JaversNativeHints {
+@ImportRuntimeHints(JaversNativeHintsConfiguration.ApplicationRuntimeHints.class)
+public class JaversNativeHintsConfiguration {
     static class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
         @Override
         public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
